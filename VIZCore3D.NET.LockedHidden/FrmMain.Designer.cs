df951844a7e72d2fd11a28c9416e535e8205b12c
softhillsdc <41414449+softhillsdc@users.noreply.github.com>
2020-01-15 16:35:01 +0900

    Add LockedHidden Example

---
+++ VIZCore3D.NET.LockedHidden/FrmMain.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace VIZCore3D.NET.LockedHidden
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dgView = new System.Windows.Forms.DataGridView();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btnDelete);
+            this.splitContainer1.Panel2.Controls.Add(this.btnClear);
+            this.splitContainer1.Panel2.Controls.Add(this.btnRefresh);
+            this.splitContainer1.Panel2.Controls.Add(this.dgView);
+            this.splitContainer1.Size = new System.Drawing.Size(1450, 826);
+            this.splitContainer1.SplitterDistance = 572;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer2.Size = new System.Drawing.Size(1450, 572);
+            this.splitContainer2.SplitterDistance = 266;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnSearch);
+            this.groupBox1.Controls.Add(this.txtKeyword);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(251, 82);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "검색";
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtKeyword.Location = new System.Drawing.Point(21, 36);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(145, 21);
+            this.txtKeyword.TabIndex = 0;
+            this.txtKeyword.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtKeyword_KeyUp);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSearch.Location = new System.Drawing.Point(185, 34);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(45, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "검색";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dgView
+            // 
+            this.dgView.AllowUserToAddRows = false;
+            this.dgView.AllowUserToDeleteRows = false;
+            this.dgView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgView.Location = new System.Drawing.Point(12, 62);
+            this.dgView.Name = "dgView";
+            this.dgView.ReadOnly = true;
+            this.dgView.RowTemplate.Height = 23;
+            this.dgView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgView.Size = new System.Drawing.Size(1426, 176);
+            this.dgView.TabIndex = 0;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(33, 21);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(297, 21);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(139, 21);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(124, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete (Selected)";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1450, 826);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "개체숨김 관리 예제";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtKeyword;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.DataGridView dgView;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ VIZCore3D.NET.LockedHidden/FrmMain.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VIZCore3D.NET.LockedHidden
+{
+    public partial class FrmMain : Form
+    {
+        // ================================================
+        // Attribute & Property
+        // ================================================
+        /// <summary>
+        /// VIZCore3D.NET
+        /// </summary>
+        private VIZCore3D.NET.VIZCore3DControl vizcore3d;
+
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            // Initialize VIZCore3D.NET
+            VIZCore3D.NET.ModuleInitializer.Run();
+
+            // Construction
+            vizcore3d = new VIZCore3DControl();
+            vizcore3d.Dock = DockStyle.Fill;
+            splitContainer2.Panel2.Controls.Add(vizcore3d);
+
+            // Event
+            vizcore3d.OnInitializedVIZCore3D += VIZCore3D_OnInitializedVIZCore3D;
+        }
+
+        // ================================================
+        // Event - VIZCore3D.NET
+        // ================================================
+        private void VIZCore3D_OnInitializedVIZCore3D(object sender, EventArgs e)
+        {
+            //MessageBox.Show("OnInitializedVIZCore3D", "VIZCore3D.NET");
+
+            // ================================================================
+            // Example
+            // ================================================================
+            //vizcore3d.License.LicenseFile("C:\\Temp\\VIZCore3D.NET.lic");                         // 라이선스 파일
+            //vizcore3d.License.LicenseServer("127.0.0.1", 8901);                                   // 라이선스 서버 - 제품 오토 선택
+            //vizcore3d.License.LicenseServer("127.0.0.1", 8901, Data.PRODUCTS.AUTO);               // 라이선스 서버 - 제품 오토 선택
+            //vizcore3d.License.LicenseServer("127.0.0.1", 8901, Data.PRODUCTS.VIZZARD_MANAGER);    // 라이선스 서버 - 지정된 제품으로 인증
+            //vizcore3d.License.LicenseServer("127.0.0.1", 8901, Data.PRODUCTS.VIZZARD_STANDARD);   // 라이선스 서버 - 지정된 제품으로 인증
+            //vizcore3d.License.LicenseServer("127.0.0.1", 8901, Data.PRODUCTS.VIZCORE3D_MANAGER);  // 라이선스 서버 - 지정된 제품으로 인증
+            //vizcore3d.License.LicenseServer("127.0.0.1", 8901, Data.PRODUCTS.VIZCORE3D_STANDARD); // 라이선스 서버 - 지정된 제품으로 인증
+
+
+            // ================================================================
+            // TEST
+            // ================================================================
+            VIZCore3D.NET.Data.LicenseResults result = vizcore3d.License.LicenseFile("C:\\License\\VIZCore3D.NET.lic");
+            //VIZCore3D.NET.Data.LicenseResults result = vizcore3d.License.LicenseServer("127.0.0.1", 8901);
+            //VIZCore3D.NET.Data.LicenseResults result = vizcore3d.License.LicenseServer("192.168.0.215", 8901);
+            if (result != Data.LicenseResults.SUCCESS)
+            {
+                MessageBox.Show(string.Format("LICENSE CODE : {0}", result.ToString()), "VIZCore3D.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Init. VIZCore3D.NET
+            InitializeVIZCore3D();
+            InitializeVIZCore3DEvent();
+
+            // Resize
+            //splitContainer1.SplitterDistance = this.Size.Height - 1000;
+        }
+
+        // ================================================
+        // Function - VIZCore3D.NET : Initialize
+        // ================================================
+        private void InitializeVIZCore3D()
+        {
+            // ================================================================
+            // 모델 열기 시, 3D 화면 Rendering 차단
+            // ================================================================
+            vizcore3d.View.BeginUpdate();
+
+
+            // ================================================================
+            // 설정 - 기본
+            // ================================================================
+            #region 설정 - 기본
+            // 모델 자동 언로드 (파일 노드 언체크 시, 언로드)
+            vizcore3d.Model.UncheckToUnload = true;
+
+            // 모델 열기 시, Edge 정보 로드 활성화
+            vizcore3d.Model.LoadEdge = true;
+
+            // 모델 열기 시, Edge 정보 변환 활성화
+            vizcore3d.Model.ConvertEdge = true;
+
+            // 모델 조회 시, 하드웨어 가속
+            vizcore3d.Option.EnableHardwareAcceleration = true;
+
+            // 모델 열기 시, 스트럭처 병합 설정
+            vizcore3d.Model.OpenMergeStructureMode = Data.MergeStructureModes.NONE;
+
+            // 모델 저장 시, 스트럭처 병합 설정
+            vizcore3d.Model.SaveMergeStructureMode = Data.MergeStructureModes.NONE;
+
+            // 실린더 원형 품질 개수 : Nomal(12~36), Small(6~36)
+            vizcore3d.Model.ReadNormalCylinderSide = 12;
+            vizcore3d.Model.ReadSmallCylinderSide = 6;
+
+            // 보이는 모델만 저장
+
+            // VIZXML to VIZ 옵션
+            vizcore3d.Model.VIZXMLtoVIZOption = Data.ExportVIZXMLToVIZOptions.LOAD_UNLOADED_NODE;
+
+            // 선택 가능 개체 : 전체, 불투명한 개체
+            vizcore3d.View.SelectionObject3DType = Data.SelectionObject3DTypes.ALL;
+
+            // 개체 선택 유형 : 색상, 경계로 선택 (개체), 경계로 선택 (전체)
+            vizcore3d.View.SelectionMode = Data.Object3DSelectionOptions.HIGHLIGHT_COLOR;
+
+            // 개체 선택 색상
+            vizcore3d.View.SelectionColor = Color.Red;
+
+            // 모델 조회 시, Pre-Select 설정
+            vizcore3d.View.EnablePreSelect = false;
+
+            // 모델 조회 시, Pre-Select 색상 설정
+            vizcore3d.View.PreSelectColor = Color.Lime;
+            #endregion
+
+
+            // ================================================================
+            // 설정 - 보기
+            // ================================================================
+            #region 설정 - 보기
+            // 자동 애니메이션 : 박스줌, 개체로 비행 등 기능에서 애니메이션 활성화/비활성화
+            vizcore3d.View.EnableAnimation = true;
+
+            // 자동화면맞춤
+            vizcore3d.View.EnableAutoFit = false;
+
+            // 연속회전모드
+            vizcore3d.View.EnableInertiaRotate = false;
+
+            // 확대/축소 비율 : 5.0f ~ 50.0f
+            vizcore3d.View.ZoomRatio = 30.0f;
+
+            // 회전각도
+            vizcore3d.View.RotationAngle = 90.0f;
+
+            // 회전 축
+            vizcore3d.View.RotationAxis = Data.Axis.X;
+            #endregion
+
+            // ================================================================
+            // 설정 - 탐색
+            // ================================================================
+            #region 설정 - 탐색
+            // Z축 고정
+            vizcore3d.View.Walkthrough.LockZAxis = true;
+            // 선속도 : m/s
+            vizcore3d.View.Walkthrough.Speed = 2.0f;
+            // 각속도
+            vizcore3d.View.Walkthrough.AngularSpeed = 30.0f;
+            // 높이
+            vizcore3d.View.Walkthrough.AvatarHeight = 1800.0f;
+            // 반지름
+            vizcore3d.View.Walkthrough.AvatarCollisionRadius = 400.0f;
+            // 숙임높이
+            vizcore3d.View.Walkthrough.AvatarBowWalkHeight = 1300.0f;
+            // 충돌
+            vizcore3d.View.Walkthrough.UseAvatarCollision = false;
+            // 중력
+            vizcore3d.View.Walkthrough.UseAvatarGravity = false;
+            // 숙임
+            vizcore3d.View.Walkthrough.UseAvatarBowWalk = false;
+            // 모델
+            vizcore3d.View.Walkthrough.AvatarModel = (int)Data.AvatarModels.MAN1;
+            // 자동줌
+            vizcore3d.View.Walkthrough.EnableAvatarAutoZoom = false;
+            // 충돌상자보기
+            vizcore3d.View.Walkthrough.ShowAvatarCollisionCylinder = false;
+            #endregion
+
+
+            // ================================================================
+            // 설정 - 조작
+            // ================================================================
+            #region 설정 - 조작
+            // 시야각
+            vizcore3d.View.FOV = 60.0f;
+            // 광원 세기
+            vizcore3d.View.SpecularGamma = 100.0f;
+            // 모서리 굵기
+            vizcore3d.View.EdgeWidthRatio = 0.0f;
+            // X-Ray 모델 조회 시, 개체 색상 - 선택색상, 모델색상
+            vizcore3d.View.XRay.ColorType = Data.XRayColorTypes.SELECTION_COLOR;
+            // 배경유형
+            //vizcore3d.View.BackgroundMode = Data.BackgroundModes.COLOR_ONE;
+            // 배경색1
+            //vizcore3d.View.BackgroundColor1 = Color.Gray;
+            // 배경색2
+            //vizcore3d.View.BackgroundColor2 = Color.Gray; 
+            #endregion
+
+            // ================================================================
+            // 설정 - 노트
+            // ================================================================
+
+
+            // ================================================================
+            // 설정 - 측정
+            // ================================================================
+
+
+
+            // ================================================================
+            // 설정 - 단면
+            // ================================================================
+            // 단면 좌표간격으로 이동
+            vizcore3d.Section.MoveSectionByFrameGrid = true;
+
+
+            // ================================================================
+            // 설정 - 간섭검사
+            // ================================================================
+            // 다중간섭검사
+            vizcore3d.Clash.EnableMultiThread = true;
+
+
+            // ================================================================
+            // 설정 - 프레임(SHIP GRID)
+            // ================================================================
+            #region 설정 - 프레임
+            // 프레임 평면 설정
+            vizcore3d.View.Frame.XYPlane = true;
+            vizcore3d.View.Frame.YZPlane = true;
+            vizcore3d.View.Frame.ZXPlane = true;
+            vizcore3d.View.Frame.PlanLine = true;
+            vizcore3d.View.Frame.SectionLine = true;
+            vizcore3d.View.Frame.ElevationLine = true;
+
+            // 좌표값 표기
+            vizcore3d.View.Frame.ShowNumber = true;
+
+            // 모델 앞에 표기
+            vizcore3d.View.Frame.BringToFront = false;
+
+            // Frame(좌표계, SHIP GRID) 색상
+            vizcore3d.View.Frame.ForeColor = Color.Black;
+
+            // 홀수번째 표시
+            vizcore3d.View.Frame.ShowOddNumber = true;
+            // 짝수번째 표시
+            vizcore3d.View.Frame.ShowEvenNumber = true;
+            // 단면상자에 자동 맞춤
+            vizcore3d.View.Frame.AutoFitSectionBox = true;
+            #endregion
+
+
+
+            // ================================================================
+            // 설정 - 툴바
+            // ================================================================
+            #region 설정 - 툴바
+            vizcore3d.ToolbarNote.Visible = false;
+            vizcore3d.ToolbarMeasurement.Visible = false;
+            vizcore3d.ToolbarSection.Visible = false;
+            vizcore3d.ToolbarClash.Visible = false;
+            vizcore3d.ToolbarAnimation.Visible = false;
+            vizcore3d.ToolbarSimulation.Visible = false;
+            #endregion
+
+            // ================================================================
+            // 설정 - 상태바
+            // ================================================================
+            vizcore3d.Statusbar.Visible = false;
+
+
+            // ================================================================
+            // 모델 열기 시, 3D 화면 Rendering 재시작
+            // ================================================================
+            vizcore3d.View.EndUpdate();
+        }
+
+        /// <summary>
+        /// 이벤트 등록
+        /// </summary>
+        private void InitializeVIZCore3DEvent()
+        {
+        }
+
+        private void RefreshList()
+        {
+            List<Data.Node> list = vizcore3d.Object3D.LockedHidden.List();
+            dgView.DataSource = list;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if(String.IsNullOrEmpty(txtKeyword.Text) == true)
+            {
+                MessageBox.Show("검색어를 입력하여 주십시오.", "VIZCore3D.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if(vizcore3d.Model.IsOpen() == false)
+            {
+                MessageBox.Show("조회중인 모델이 없습니다.", "VIZCore3D.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            List<Data.Node> node = vizcore3d.Object3D.Find.QuickSearch(
+                new List<string>(){ txtKeyword.Text } 
+                , false     /* OR 검색 */
+                , true      /* Assembly Only */
+                , false     /* All Node */
+                , false     /* All Node */
+                , false     /* Contains */
+                );
+
+            if(node.Count == 0)
+            {
+                MessageBox.Show("결과가 없습니다.", "VIZCore3D.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // 잠금숨김 추가
+            vizcore3d.Object3D.LockedHidden.Add(node);
+
+            // 목록 갱신
+            RefreshList();
+        }
+
+        private void txtKeyword_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                btnSearch.PerformClick();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // 개체숨김 초기화
+            vizcore3d.Object3D.LockedHidden.Clear();
+
+            // 목록 갱신
+            RefreshList();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int selectedRowCount = dgView.Rows.GetRowCount(DataGridViewElementStates.Selected);
+
+            if(selectedRowCount == 0)
+            {
+                MessageBox.Show("선택된 항목이 없습니다.", "VIZCore3D.NET", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            List<int> index = new List<int>();
+
+            for (int i = 0; i < selectedRowCount; i++)
+            {
+                int val = Convert.ToInt32(dgView.SelectedRows[i].Cells[1].Value);
+                index.Add(val);
+            }
+
+            vizcore3d.Object3D.LockedHidden.Delete(index);
+
+            // 목록 갱신
+            RefreshList();
+        }
+    }
+}
