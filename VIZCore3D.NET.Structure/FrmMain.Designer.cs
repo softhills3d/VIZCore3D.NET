0c645d7d0e2cb6d9c88ce35447135e9e37923685
softhillsdc <41414449+softhillsdc@users.noreply.github.com>
2020-01-28 00:59:08 +0900

    Add Structure Example

---
+++ VIZCore3D.NET.Structure/FrmMain.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace VIZCore3D.NET.Structure
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lvProperty = new System.Windows.Forms.ListView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tvStructure = new System.Windows.Forms.TreeView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnVIZ = new System.Windows.Forms.Button();
+            this.txtVIZ = new System.Windows.Forms.TextBox();
+            this.imgList = new System.Windows.Forms.ImageList(this.components);
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox3);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1125, 626);
+            this.splitContainer1.SplitterDistance = 487;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.lvProperty);
+            this.groupBox3.Location = new System.Drawing.Point(12, 375);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(458, 239);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Property";
+            // 
+            // lvProperty
+            // 
+            this.lvProperty.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvProperty.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.lvProperty.FullRowSelect = true;
+            this.lvProperty.GridLines = true;
+            this.lvProperty.HideSelection = false;
+            this.lvProperty.Location = new System.Drawing.Point(6, 20);
+            this.lvProperty.Name = "lvProperty";
+            this.lvProperty.Size = new System.Drawing.Size(446, 213);
+            this.lvProperty.TabIndex = 0;
+            this.lvProperty.UseCompatibleStateImageBehavior = false;
+            this.lvProperty.View = System.Windows.Forms.View.Details;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.tvStructure);
+            this.groupBox2.Location = new System.Drawing.Point(12, 117);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(458, 252);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Structure";
+            // 
+            // tvStructure
+            // 
+            this.tvStructure.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tvStructure.ImageIndex = 0;
+            this.tvStructure.ImageList = this.imgList;
+            this.tvStructure.Location = new System.Drawing.Point(6, 20);
+            this.tvStructure.Name = "tvStructure";
+            this.tvStructure.SelectedImageIndex = 0;
+            this.tvStructure.Size = new System.Drawing.Size(446, 226);
+            this.tvStructure.TabIndex = 0;
+            this.tvStructure.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvStructure_AfterSelect);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnVIZ);
+            this.groupBox1.Controls.Add(this.txtVIZ);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(458, 99);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "VIZ";
+            // 
+            // btnVIZ
+            // 
+            this.btnVIZ.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnVIZ.Location = new System.Drawing.Point(368, 38);
+            this.btnVIZ.Name = "btnVIZ";
+            this.btnVIZ.Size = new System.Drawing.Size(75, 23);
+            this.btnVIZ.TabIndex = 3;
+            this.btnVIZ.Text = "Select";
+            this.btnVIZ.UseVisualStyleBackColor = true;
+            this.btnVIZ.Click += new System.EventHandler(this.btnVIZ_Click);
+            // 
+            // txtVIZ
+            // 
+            this.txtVIZ.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtVIZ.Location = new System.Drawing.Point(29, 40);
+            this.txtVIZ.Name = "txtVIZ";
+            this.txtVIZ.ReadOnly = true;
+            this.txtVIZ.Size = new System.Drawing.Size(333, 21);
+            this.txtVIZ.TabIndex = 2;
+            // 
+            // imgList
+            // 
+            this.imgList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgList.ImageStream")));
+            this.imgList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgList.Images.SetKeyName(0, "MODEL.png");
+            this.imgList.Images.SetKeyName(1, "ASSEMBLY.png");
+            this.imgList.Images.SetKeyName(2, "PART.png");
+            this.imgList.Images.SetKeyName(3, "BODY.png");
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Key";
+            this.columnHeader1.Width = 135;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Value";
+            this.columnHeader2.Width = 167;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1125, 626);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VIZCore3D.NET.Structure";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnVIZ;
+        private System.Windows.Forms.TextBox txtVIZ;
+        private System.Windows.Forms.TreeView tvStructure;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListView lvProperty;
+        private System.Windows.Forms.ImageList imgList;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ VIZCore3D.NET.Structure/FrmMain.cs
@@ -0,0 +1,451 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VIZCore3D.NET.Structure
+{
+    public partial class FrmMain : Form
+    {
+        // ================================================
+        // Attribute & Property
+        // ================================================
+        /// <summary>
+        /// VIZCore3D.NET
+        /// </summary>
+        private VIZCore3D.NET.VIZCore3DControl vizcore3d;
+
+        /// <summary>
+        /// 모델 구조(Structure) 처리기
+        /// </summary>
+        public VIZCore3D.NET.Data.ShStructure StructureManager { get; set; }
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            // Initialize VIZCore3D.NET
+            VIZCore3D.NET.ModuleInitializer.Run();
+
+            // Construction
+            vizcore3d = new VIZCore3DControl();
+            vizcore3d.Dock = DockStyle.Fill;
+            splitContainer1.Panel2.Controls.Add(vizcore3d);
+
+            // Event
+            vizcore3d.OnInitializedVIZCore3D += VIZCore3D_OnInitializedVIZCore3D;
+        }
+
+        // ================================================
+        // Event - VIZCore3D.NET
+        // ================================================
+        private void VIZCore3D_OnInitializedVIZCore3D(object sender, EventArgs e)
+        {
+            //MessageBox.Show("OnInitializedVIZCore3D", "VIZCore3D.NET");
+
+            // ================================================================
+            // Example
+            // ================================================================
+            //vizcore3d.License.LicenseFile("C:\\Temp\\VIZCore3D.NET.lic");                         // 라이선스 파일
+            //vizcore3d.License.LicenseServer("127.0.0.1", 8901);                                   // 라이선스 서버 - 제품 오토 선택
+            //vizcore3d.License.LicenseServer("127.0.0.1", 8901, Data.Products.AUTO);               // 라이선스 서버 - 제품 오토 선택
+            //vizcore3d.License.LicenseServer("127.0.0.1", 8901, Data.Products.VIZZARD_MANAGER);    // 라이선스 서버 - 지정된 제품으로 인증
+            //vizcore3d.License.LicenseServer("127.0.0.1", 8901, Data.Products.VIZZARD_STANDARD);   // 라이선스 서버 - 지정된 제품으로 인증
+            //vizcore3d.License.LicenseServer("127.0.0.1", 8901, Data.Products.VIZCORE3D_MANAGER);  // 라이선스 서버 - 지정된 제품으로 인증
+            //vizcore3d.License.LicenseServer("127.0.0.1", 8901, Data.Products.VIZCORE3D_STANDARD); // 라이선스 서버 - 지정된 제품으로 인증
+
+
+            // ================================================================
+            // TEST
+            // ================================================================
+            VIZCore3D.NET.Data.LicenseResults result = vizcore3d.License.LicenseFile("C:\\License\\VIZCore3D.NET.lic");
+            //VIZCore3D.NET.Data.LicenseResults result = vizcore3d.License.LicenseServer("127.0.0.1", 8901);
+            //VIZCore3D.NET.Data.LicenseResults result = vizcore3d.License.LicenseServer("192.168.0.215", 8901);
+            if (result != Data.LicenseResults.SUCCESS)
+            {
+                MessageBox.Show(string.Format("LICENSE CODE : {0}", result.ToString()), "VIZCore3D.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Init. VIZCore3D.NET
+            InitializeVIZCore3D();
+            InitializeVIZCore3DEvent();
+        }
+
+        // ================================================
+        // Function - VIZCore3D.NET : Initialize
+        // ================================================
+        private void InitializeVIZCore3D()
+        {
+            // ================================================================
+            // 모델 열기 시, 3D 화면 Rendering 차단
+            // ================================================================
+            vizcore3d.View.BeginUpdate();
+
+
+            // ================================================================
+            // 설정 - 기본
+            // ================================================================
+            #region 설정 - 기본
+            // 모델 자동 언로드 (파일 노드 언체크 시, 언로드)
+            vizcore3d.Model.UncheckToUnload = true;
+
+            // 모델 열기 시, Edge 정보 로드 활성화
+            vizcore3d.Model.LoadEdge = true;
+
+            // 모델 열기 시, Edge 정보 변환 활성화
+            vizcore3d.Model.ConvertEdge = true;
+
+            // 모델 조회 시, 하드웨어 가속
+            vizcore3d.View.EnableHardwareAcceleration = true;
+
+            // 모델 열기 시, 스트럭처 병합 설정
+            vizcore3d.Model.OpenMergeStructureMode = Data.MergeStructureModes.NONE;
+
+            // 모델 저장 시, 스트럭처 병합 설정
+            vizcore3d.Model.SaveMergeStructureMode = Data.MergeStructureModes.NONE;
+
+            // 실린더 원형 품질 개수 : Nomal(12~36), Small(6~36)
+            vizcore3d.Model.ReadNormalCylinderSide = 12;
+            vizcore3d.Model.ReadSmallCylinderSide = 6;
+
+            // 보이는 모델만 저장
+
+            // VIZXML to VIZ 옵션
+            vizcore3d.Model.VIZXMLtoVIZOption = Data.ExportVIZXMLToVIZOptions.LOAD_UNLOADED_NODE;
+
+            // 선택 가능 개체 : 전체, 불투명한 개체
+            vizcore3d.View.SelectionObject3DType = Data.SelectionObject3DTypes.ALL;
+
+            // 개체 선택 유형 : 색상, 경계로 선택 (개체), 경계로 선택 (전체)
+            vizcore3d.View.SelectionMode = Data.Object3DSelectionOptions.HIGHLIGHT_COLOR;
+
+            // 개체 선택 색상
+            vizcore3d.View.SelectionColor = Color.Red;
+
+            // 모델 조회 시, Pre-Select 설정
+            vizcore3d.View.EnablePreSelect = false;
+
+            // 모델 조회 시, Pre-Select 색상 설정
+            vizcore3d.View.PreSelectColor = Color.Lime;
+            #endregion
+
+
+            // ================================================================
+            // 설정 - 보기
+            // ================================================================
+            #region 설정 - 보기
+            // 자동 애니메이션 : 박스줌, 개체로 비행 등 기능에서 애니메이션 활성화/비활성화
+            vizcore3d.View.EnableAnimation = true;
+
+            // 자동화면맞춤
+            vizcore3d.View.EnableAutoFit = false;
+
+            // 연속회전모드
+            vizcore3d.View.EnableInertiaRotate = false;
+
+            // 확대/축소 비율 : 5.0f ~ 50.0f
+            vizcore3d.View.ZoomRatio = 30.0f;
+
+            // 회전각도
+            vizcore3d.View.RotationAngle = 90.0f;
+
+            // 회전 축
+            vizcore3d.View.RotationAxis = Data.Axis.X;
+            #endregion
+
+            // ================================================================
+            // 설정 - 탐색
+            // ================================================================
+            #region 설정 - 탐색
+            // Z축 고정
+            vizcore3d.View.Walkthrough.LockZAxis = true;
+            // 선속도 : m/s
+            vizcore3d.View.Walkthrough.Speed = 2.0f;
+            // 각속도
+            vizcore3d.View.Walkthrough.AngularSpeed = 30.0f;
+            // 높이
+            vizcore3d.View.Walkthrough.AvatarHeight = 1800.0f;
+            // 반지름
+            vizcore3d.View.Walkthrough.AvatarCollisionRadius = 400.0f;
+            // 숙임높이
+            vizcore3d.View.Walkthrough.AvatarBowWalkHeight = 1300.0f;
+            // 충돌
+            vizcore3d.View.Walkthrough.UseAvatarCollision = false;
+            // 중력
+            vizcore3d.View.Walkthrough.UseAvatarGravity = false;
+            // 숙임
+            vizcore3d.View.Walkthrough.UseAvatarBowWalk = false;
+            // 모델
+            vizcore3d.View.Walkthrough.AvatarModel = (int)Data.AvatarModels.MAN1;
+            // 자동줌
+            vizcore3d.View.Walkthrough.EnableAvatarAutoZoom = false;
+            // 충돌상자보기
+            vizcore3d.View.Walkthrough.ShowAvatarCollisionCylinder = false;
+            #endregion
+
+
+            // ================================================================
+            // 설정 - 조작
+            // ================================================================
+            #region 설정 - 조작
+            // 시야각
+            vizcore3d.View.FOV = 60.0f;
+            // 광원 세기
+            vizcore3d.View.SpecularGamma = 100.0f;
+            // 모서리 굵기
+            vizcore3d.View.EdgeWidthRatio = 0.0f;
+            // X-Ray 모델 조회 시, 개체 색상 - 선택색상, 모델색상
+            vizcore3d.View.XRay.ColorType = Data.XRayColorTypes.SELECTION_COLOR;
+            // 배경유형
+            //vizcore3d.View.BackgroundMode = Data.BackgroundModes.COLOR_ONE;
+            // 배경색1
+            //vizcore3d.View.BackgroundColor1 = Color.Gray;
+            // 배경색2
+            //vizcore3d.View.BackgroundColor2 = Color.Gray; 
+            #endregion
+
+            // ================================================================
+            // 설정 - 노트
+            // ================================================================
+
+
+            // ================================================================
+            // 설정 - 측정
+            // ================================================================
+
+
+
+            // ================================================================
+            // 설정 - 단면
+            // ================================================================
+            // 단면 좌표간격으로 이동
+            vizcore3d.Section.MoveSectionByFrameGrid = true;
+
+
+            // ================================================================
+            // 설정 - 간섭검사
+            // ================================================================
+            // 다중간섭검사
+            vizcore3d.Clash.EnableMultiThread = true;
+
+
+            // ================================================================
+            // 설정 - 프레임(SHIP GRID)
+            // ================================================================
+            #region 설정 - 프레임
+            // 프레임 평면 설정
+            vizcore3d.View.Frame.XYPlane = true;
+            vizcore3d.View.Frame.YZPlane = true;
+            vizcore3d.View.Frame.ZXPlane = true;
+            vizcore3d.View.Frame.PlanLine = true;
+            vizcore3d.View.Frame.SectionLine = true;
+            vizcore3d.View.Frame.ElevationLine = true;
+
+            // 좌표값 표기
+            vizcore3d.View.Frame.ShowNumber = true;
+
+            // 모델 앞에 표기
+            vizcore3d.View.Frame.BringToFront = false;
+
+            // Frame(좌표계, SHIP GRID) 색상
+            vizcore3d.View.Frame.ForeColor = Color.Black;
+
+            // 홀수번째 표시
+            vizcore3d.View.Frame.ShowOddNumber = true;
+            // 짝수번째 표시
+            vizcore3d.View.Frame.ShowEvenNumber = true;
+            // 단면상자에 자동 맞춤
+            vizcore3d.View.Frame.AutoFitSectionBox = true;
+            #endregion
+
+
+
+            // ================================================================
+            // 설정 - 툴바
+            // ================================================================
+            #region 설정 - 툴바
+            vizcore3d.ToolbarNote.Visible = false;
+            vizcore3d.ToolbarMeasurement.Visible = false;
+            vizcore3d.ToolbarSection.Visible = false;
+            vizcore3d.ToolbarClash.Visible = false;
+            vizcore3d.ToolbarAnimation.Visible = false;
+            vizcore3d.ToolbarSimulation.Visible = false;
+            #endregion
+
+            // ================================================================
+            // 설정 - 상태바
+            // ================================================================
+            vizcore3d.Statusbar.Visible = true;
+
+
+            // ================================================================
+            // 모델 열기 시, 3D 화면 Rendering 재시작
+            // ================================================================
+            vizcore3d.View.EndUpdate();
+        }
+
+        /// <summary>
+        /// 이벤트 등록
+        /// </summary>
+        private void InitializeVIZCore3DEvent()
+        {
+        }
+
+        private void btnVIZ_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "VIZ (*.viz)|*.viz";
+            if (dlg.ShowDialog() != DialogResult.OK) return;
+
+            ShowStructure(dlg.FileName);
+        }
+
+        private void ShowStructure(string path)
+        {
+            txtVIZ.Text = path;
+
+            // 모델 로드
+            StructureManager = new Data.ShStructure(path, false);
+
+            MakeModelTree();
+        }
+
+        private void MakeModelTree()
+        {
+            tvStructure.BeginUpdate();
+            tvStructure.Nodes.Clear();
+
+            // 처리된 모델 구조 불러오기
+            List<VIZCore3D.NET.Data.ShModelTreeNode> roots = StructureManager.Roots;
+
+            // 최상위는 파일 노드이므로, 파일이름으로 생성
+            TreeNode rootNode = tvStructure.Nodes.Add(System.IO.Path.GetFileNameWithoutExtension(txtVIZ.Text));
+            rootNode.ImageIndex = 0;
+            rootNode.SelectedImageIndex = 0;
+
+            // 최상위 루트 기준
+            for (int i = 0; i < roots.Count; i++)
+            {
+                // 파일 노드 하위의 최상위 노드
+                VIZCore3D.NET.Data.ShModelTreeNode node = roots[i];
+
+                TreeNode tnNode = rootNode.Nodes.Add(node.NodeName);
+                tnNode.Tag = node;
+
+                if (node.NodeType == VIZCore3D.NET.Data.ShModelTreeNodeType.ASSEMBLY)
+                {
+                    tnNode.ImageIndex = 1;
+                    tnNode.SelectedImageIndex = 1;
+                }
+                else if (node.NodeType == VIZCore3D.NET.Data.ShModelTreeNodeType.PART)
+                {
+                    tnNode.ImageIndex = 2;
+                    tnNode.SelectedImageIndex = 2;
+                }
+                else if (node.NodeType == VIZCore3D.NET.Data.ShModelTreeNodeType.BODY)
+                {
+                    tnNode.ImageIndex = 3;
+                    tnNode.SelectedImageIndex = 3;
+                }
+
+                // 하위 노드가 있는 경우, 재귀 호출
+                if (node.Nodes.Count != 0)
+                {
+                    BindNode(tnNode, node);
+                }
+            }
+
+            // 최상위 노드 펼치기
+            rootNode.Expand();
+
+            tvStructure.EndUpdate();
+        }
+
+        /// <summary>
+        /// 하위 노드를 트리 컨트롤에 추가
+        /// </summary>
+        /// <param name="tnNode"></param>
+        /// <param name="mtNode"></param>
+        private void BindNode(TreeNode tnNode, VIZCore3D.NET.Data.ShModelTreeNode mtNode)
+        {
+            // 부모 노드의 하위 자식 목록
+            List<VIZCore3D.NET.Data.ShModelTreeNode> children = mtNode.Nodes;
+
+            for (int i = 0; i < children.Count; i++)
+            {
+                // 노드 정보
+                VIZCore3D.NET.Data.ShModelTreeNode node = children[i];
+
+                // 트리 노드 생성
+                TreeNode childNode = tnNode.Nodes.Add(node.NodeName);
+                childNode.Tag = node;
+
+                if (node.NodeType == VIZCore3D.NET.Data.ShModelTreeNodeType.ASSEMBLY)
+                {
+                    childNode.ImageIndex = 1;
+                    childNode.SelectedImageIndex = 1;
+                }
+                else if (node.NodeType == VIZCore3D.NET.Data.ShModelTreeNodeType.PART)
+                {
+                    childNode.ImageIndex = 2;
+                    childNode.SelectedImageIndex = 2;
+                }
+                else if (node.NodeType == VIZCore3D.NET.Data.ShModelTreeNodeType.BODY)
+                {
+                    childNode.ImageIndex = 3;
+                    childNode.SelectedImageIndex = 3;
+                }
+
+                // 하위 노드가 있는 경우, 재귀 호출
+                if (node.Nodes.Count != 0)
+                {
+                    BindNode(childNode, node);
+                }
+            }
+        }
+
+        private void tvStructure_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Tag == null) return;
+
+            VIZCore3D.NET.Data.ShModelTreeNode node = (VIZCore3D.NET.Data.ShModelTreeNode)e.Node.Tag;
+
+            // 선택된 노드의 속성정보 불러오기
+            // Example 1
+            List<VIZCore3D.NET.Data.ShNodeAttribute> AttrItems1 = StructureManager.GetProperty(Convert.ToInt32(node.EntityId));
+            // Example 2
+            List<VIZCore3D.NET.Data.ShNodeAttribute> AttrItems2 = node.NodePropertyList;
+
+            ShowProperty(AttrItems2);
+        }
+
+        private void ShowProperty(List<VIZCore3D.NET.Data.ShNodeAttribute> items)
+        {
+            lvProperty.BeginUpdate();
+            lvProperty.Items.Clear();
+            foreach (VIZCore3D.NET.Data.ShNodeAttribute item in items)
+            {
+                ListViewItem lvi = new ListViewItem(new string[] { item.Key, item.Value });
+                lvProperty.Items.Add(lvi);
+            }
+            lvProperty.EndUpdate();
+        }
+    }
+
+
+    public class ModelNodeVO
+    {
+        public long NodeId { get; set; }
+        public string NodeName { get; set; }
+        public string NodeType { get; set; }
+        public string ParentNodeName { get; set; }
+        public int ChildrenCount { get; set; }
+        public int Depth { get; set; }
+        public string NodePath { get; set; }
+    }
+}
