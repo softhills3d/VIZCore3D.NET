8cd1ae468cdb314892f04879200abaccb14c3cd3
softhillsdc <41414449+softhillsdc@users.noreply.github.com>
2020-03-19 00:41:41 +0900

    Rename API

--- VIZCore3D.NET.SelectionBox/FrmMain.cs
+++ VIZCore3D.NET.SelectionBox/FrmMain.cs
@@ -347,7 +347,7 @@ namespace VIZCore3D.NET.SelectionBox
 
         private void SelectionBox_OnSelectionBoxDragEvent(object sender, VIZCore3D.NET.Event.EventManager.SelectionBoxEventArgs e)
         {
-            VIZCore3D.NET.Data.SelectionBox info = vizcore3d.SelectionBox.GetData(e.ID);
+            VIZCore3D.NET.Data.SelectionBox info = vizcore3d.SelectionBox.GetItem(e.ID);
 
             vizcore3d.View.Message.Show(Data.MessageId.ID_05, string.Format("Size : {0} / {1}", info.MinPoint.ToString(), info.MaxPoint.ToString()), 5, 65, Data.FontSize.Size_14, Color.Blue);
         }
@@ -553,7 +553,7 @@ namespace VIZCore3D.NET.SelectionBox
 
         private void DisplayMessage(int id)
         {
-            VIZCore3D.NET.Data.SelectionBox info = vizcore3d.SelectionBox.GetData(id);
+            VIZCore3D.NET.Data.SelectionBox info = vizcore3d.SelectionBox.GetItem(id);
             VIZCore3D.NET.Data.Vertex3D cog = vizcore3d.SelectionBox.GetCOG(id, Data.BoundBoxSearchOption.FullyContained);
 
             vizcore3d.View.Message.Show(VIZCore3D.NET.Data.MessageId.ID_01, "선택상자", 5, 5, VIZCore3D.NET.Data.FontSize.Size_14_Bold, Color.Red);
