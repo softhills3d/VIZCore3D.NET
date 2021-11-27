a2c07313974038b0325f41fa89bd03411601bd20
SOFTHILLS <41414449+softhills3d@users.noreply.github.com>
2021-11-27 09:22:49 +0900

    structural change

---
+++ WinForms/VIZCore3D.NET.SimplifyBlockByPart/FrmMain.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace VIZCore3D.NET.SimplifyBlockByPart
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnSimplifyPart = new System.Windows.Forms.Button();
+            this.rbTriangle = new System.Windows.Forms.RadioButton();
+            this.rbTriangleSet = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnGenerateTree = new System.Windows.Forms.Button();
+            this.btnLinkModel = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btnExportModel = new System.Windows.Forms.Button();
+            this.btnShowParts = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox4);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox3);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1123, 541);
+            this.splitContainer1.SplitterDistance = 374;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnSelect);
+            this.groupBox1.Controls.Add(this.txtInput);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(353, 72);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "INPUT";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInput.Location = new System.Drawing.Point(20, 32);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ReadOnly = true;
+            this.txtInput.Size = new System.Drawing.Size(239, 21);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelect.Location = new System.Drawing.Point(265, 30);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.rbTriangleSet);
+            this.groupBox2.Controls.Add(this.rbTriangle);
+            this.groupBox2.Controls.Add(this.btnSimplifyPart);
+            this.groupBox2.Controls.Add(this.btnExport);
+            this.groupBox2.Location = new System.Drawing.Point(12, 90);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(353, 98);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Task";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(20, 31);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(90, 23);
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "Export Part";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // btnSimplifyPart
+            // 
+            this.btnSimplifyPart.Location = new System.Drawing.Point(125, 31);
+            this.btnSimplifyPart.Name = "btnSimplifyPart";
+            this.btnSimplifyPart.Size = new System.Drawing.Size(90, 23);
+            this.btnSimplifyPart.TabIndex = 2;
+            this.btnSimplifyPart.Text = "Simplify Part";
+            this.btnSimplifyPart.UseVisualStyleBackColor = true;
+            this.btnSimplifyPart.Click += new System.EventHandler(this.btnSimplifyPart_Click);
+            // 
+            // rbTriangle
+            // 
+            this.rbTriangle.AutoSize = true;
+            this.rbTriangle.Location = new System.Drawing.Point(20, 66);
+            this.rbTriangle.Name = "rbTriangle";
+            this.rbTriangle.Size = new System.Drawing.Size(69, 16);
+            this.rbTriangle.TabIndex = 3;
+            this.rbTriangle.Text = "Triangle";
+            this.rbTriangle.UseVisualStyleBackColor = true;
+            // 
+            // rbTriangleSet
+            // 
+            this.rbTriangleSet.AutoSize = true;
+            this.rbTriangleSet.Checked = true;
+            this.rbTriangleSet.Location = new System.Drawing.Point(105, 66);
+            this.rbTriangleSet.Name = "rbTriangleSet";
+            this.rbTriangleSet.Size = new System.Drawing.Size(91, 16);
+            this.rbTriangleSet.TabIndex = 4;
+            this.rbTriangleSet.TabStop = true;
+            this.rbTriangleSet.Text = "Triangle Set";
+            this.rbTriangleSet.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.btnLinkModel);
+            this.groupBox3.Controls.Add(this.btnGenerateTree);
+            this.groupBox3.Location = new System.Drawing.Point(12, 194);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(353, 72);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "MODEL TREE";
+            // 
+            // btnGenerateTree
+            // 
+            this.btnGenerateTree.Location = new System.Drawing.Point(20, 29);
+            this.btnGenerateTree.Name = "btnGenerateTree";
+            this.btnGenerateTree.Size = new System.Drawing.Size(90, 23);
+            this.btnGenerateTree.TabIndex = 2;
+            this.btnGenerateTree.Text = "Generate";
+            this.btnGenerateTree.UseVisualStyleBackColor = true;
+            this.btnGenerateTree.Click += new System.EventHandler(this.btnGenerateTree_Click);
+            // 
+            // btnLinkModel
+            // 
+            this.btnLinkModel.Location = new System.Drawing.Point(125, 29);
+            this.btnLinkModel.Name = "btnLinkModel";
+            this.btnLinkModel.Size = new System.Drawing.Size(90, 23);
+            this.btnLinkModel.TabIndex = 3;
+            this.btnLinkModel.Text = "Link Model";
+            this.btnLinkModel.UseVisualStyleBackColor = true;
+            this.btnLinkModel.Click += new System.EventHandler(this.btnLinkModel_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox4.Controls.Add(this.btnExportModel);
+            this.groupBox4.Controls.Add(this.btnShowParts);
+            this.groupBox4.Location = new System.Drawing.Point(12, 272);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(353, 72);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "MODEL";
+            // 
+            // btnExportModel
+            // 
+            this.btnExportModel.Location = new System.Drawing.Point(125, 29);
+            this.btnExportModel.Name = "btnExportModel";
+            this.btnExportModel.Size = new System.Drawing.Size(90, 23);
+            this.btnExportModel.TabIndex = 3;
+            this.btnExportModel.Text = "Export";
+            this.btnExportModel.UseVisualStyleBackColor = true;
+            this.btnExportModel.Click += new System.EventHandler(this.btnExportModel_Click);
+            // 
+            // btnShowParts
+            // 
+            this.btnShowParts.Location = new System.Drawing.Point(20, 29);
+            this.btnShowParts.Name = "btnShowParts";
+            this.btnShowParts.Size = new System.Drawing.Size(90, 23);
+            this.btnShowParts.TabIndex = 2;
+            this.btnShowParts.Text = "Show";
+            this.btnShowParts.UseVisualStyleBackColor = true;
+            this.btnShowParts.Click += new System.EventHandler(this.btnShowParts_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1123, 541);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simplify Block By Part";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnSimplifyPart;
+        private System.Windows.Forms.RadioButton rbTriangleSet;
+        private System.Windows.Forms.RadioButton rbTriangle;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnGenerateTree;
+        private System.Windows.Forms.Button btnLinkModel;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btnExportModel;
+        private System.Windows.Forms.Button btnShowParts;
+    }
+}
+
