1135b3b813fbb90d4a3dc29c38d17c07b610e357
SOFTHILLS <41414449+softhills3d@users.noreply.github.com>
2021-12-15 20:08:21 +0900

    New

---
+++ WinForms/VIZCore3D.NET.ProjectionGrid/FrmMain.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace VIZCore3D.NET.ProjectionGrid
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnScreenCoord = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 57);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Size = new System.Drawing.Size(1123, 517);
+            this.splitContainer1.SplitterDistance = 571;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // btnScreenCoord
+            // 
+            this.btnScreenCoord.Location = new System.Drawing.Point(12, 12);
+            this.btnScreenCoord.Name = "btnScreenCoord";
+            this.btnScreenCoord.Size = new System.Drawing.Size(143, 23);
+            this.btnScreenCoord.TabIndex = 1;
+            this.btnScreenCoord.Text = "Get Screen Coord.";
+            this.btnScreenCoord.UseVisualStyleBackColor = true;
+            this.btnScreenCoord.Click += new System.EventHandler(this.btnScreenCoord_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1147, 586);
+            this.Controls.Add(this.btnScreenCoord);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VIZCore3D.NET.ProjectionGrid";
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnScreenCoord;
+    }
+}
+
---
+++ WinForms/VIZCore3D.NET.ProjectionGrid/GridPanelControl.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VIZCore3D.NET.ProjectionGrid
+{
+    public partial class GridPanelControl : UserControl
+    {
+        private List<System.Drawing.PointF> Points { get; set; }
+
+        public GridPanelControl()
+        {
+            InitializeComponent();
+        }
+
+        public void SetData(List<System.Drawing.PointF> points)
+        {
+            Points = points;
+
+            Invalidate();
+        }
+
+        private void GridPanelControl_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            int width = this.Size.Width;
+            int height = this.Size.Height;
+
+            Pen pen = new Pen(Brushes.Black);
+
+            for (int i = 1; i < width; i++)
+            {
+                e.Graphics.DrawLine(
+                    pen
+                    , new Point(i, 0)
+                    , new Point(i, height)
+                    );
+
+                i += 4;
+            }
+
+            for (int i = 1; i < height; i++)
+            {
+                e.Graphics.DrawLine(
+                    pen
+                    , new Point(0, i)
+                    , new Point(width, i)
+                    );
+
+                i += 4;
+            }
+
+            pen.Dispose();
+
+            if (Points != null && Points.Count != 0)
+            {
+                Pen p = new Pen(Brushes.Red, 0.1f);
+                for (int i = 0; i < Points.Count; i++)
+                {
+                    System.Drawing.PointF p1 = Points[i];
+                    System.Drawing.PointF p2 = new PointF(p1.X + 2.0f, p1.Y + 2.0f);
+                    e.Graphics.DrawLine(p, p1, p2);
+                }
+                p.Dispose();
+            }
+        }
+    }
+}
