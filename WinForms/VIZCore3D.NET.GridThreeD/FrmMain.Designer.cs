5fe9e0895f0ee53ede4f8c50325fa38d948273ca
Softhills Co., Ltd <41414449+softhills3d@users.noreply.github.com>
2022-04-28 13:42:12 +0900

    3D Grid

---
+++ WinForms/VIZCore3D.NET.GridThreeD/FrmMain.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace VIZCore3D.NET.GridThreeD
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer4 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer4)).BeginInit();
+            this.splitContainer4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(1178, 557);
+            this.splitContainer1.SplitterDistance = 230;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.splitContainer3);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.splitContainer4);
+            this.splitContainer2.Size = new System.Drawing.Size(944, 557);
+            this.splitContainer2.SplitterDistance = 303;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Size = new System.Drawing.Size(944, 303);
+            this.splitContainer3.SplitterDistance = 439;
+            this.splitContainer3.TabIndex = 0;
+            // 
+            // splitContainer4
+            // 
+            this.splitContainer4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer4.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer4.Name = "splitContainer4";
+            this.splitContainer4.Size = new System.Drawing.Size(944, 250);
+            this.splitContainer4.SplitterDistance = 438;
+            this.splitContainer4.TabIndex = 0;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 557);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VIZCore3D.NET - 3D Grid";
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer4)).EndInit();
+            this.splitContainer4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.SplitContainer splitContainer4;
+    }
+}
+
---
+++ WinForms/VIZCore3D.NET.GridThreeD/FrmMain.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VIZCore3D.NET.GridThreeD
+{
+    public partial class FrmMain : Form
+    {
+        // ================================================
+        // Attribute & Property
+        // ================================================
+        /// <summary>
+        /// VIZCore3D.NET
+        /// </summary>
+        private VIZCore3D.NET.VIZCore3DControl vizcore3dMain;
+        private VIZCore3D.NET.VIZCore3DControl vizcore3dDisassembly;
+        private VIZCore3D.NET.VIZCore3DControl vizcore3dGrid;
+        private VIZCore3D.NET.VIZCore3DControl vizcore3dView;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            // Initialize VIZCore3D.NET
+            VIZCore3D.NET.ModuleInitializer.Run();
+
+            // Construction
+            {
+                vizcore3dMain = new VIZCore3D.NET.VIZCore3DControl();
+
+                vizcore3dMain.Dock = DockStyle.Fill;
+                splitContainer3.Panel1.Controls.Add(vizcore3dMain);
+            }
+
+            {
+                vizcore3dDisassembly = new VIZCore3D.NET.VIZCore3DControl();
+
+                vizcore3dDisassembly.Dock = DockStyle.Fill;
+                splitContainer3.Panel2.Controls.Add(vizcore3dDisassembly);
+            }
+
+            {
+                vizcore3dGrid = new VIZCore3D.NET.VIZCore3DControl();
+
+                vizcore3dGrid.Dock = DockStyle.Fill;
+                splitContainer4.Panel1.Controls.Add(vizcore3dGrid);
+            }
+
+            {
+                vizcore3dView = new VIZCore3D.NET.VIZCore3DControl();
+
+                vizcore3dView.Dock = DockStyle.Fill;
+                splitContainer4.Panel2.Controls.Add(vizcore3dView);
+            }
+
+
+            // Event
+            vizcore3dMain.OnInitializedVIZCore3D += VIZCore3D_Main_OnInitializedVIZCore3D;
+            vizcore3dDisassembly.OnInitializedVIZCore3D += VIZCore3D_Disassembly_OnInitializedVIZCore3D;
+            vizcore3dGrid.OnInitializedVIZCore3D += VIZCore3D_Grid_OnInitializedVIZCore3D;
+            vizcore3dView.OnInitializedVIZCore3D += VIZCore3D_View_OnInitializedVIZCore3D;
+        }
+
+        // ================================================
+        // Event - VIZCore3D.NET
+        // ================================================
+        #region Event - OnInitializedVIZCore3D
+        private void VIZCore3D_Main_OnInitializedVIZCore3D(object sender, EventArgs e)
+        {
+            VIZCore3DHelper.OnInitializedVIZCore3D(vizcore3dMain);
+        }
+
+        private void VIZCore3D_Disassembly_OnInitializedVIZCore3D(object sender, EventArgs e)
+        {
+            VIZCore3DHelper.OnInitializedVIZCore3D(vizcore3dDisassembly);
+        }
+
+        private void VIZCore3D_Grid_OnInitializedVIZCore3D(object sender, EventArgs e)
+        {
+            VIZCore3DHelper.OnInitializedVIZCore3D(vizcore3dGrid);
+        }
+
+        private void VIZCore3D_View_OnInitializedVIZCore3D(object sender, EventArgs e)
+        {
+            VIZCore3DHelper.OnInitializedVIZCore3D(vizcore3dView);
+        }
+        #endregion
+
+
+    }
+}
