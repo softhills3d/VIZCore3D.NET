e788de6915a24c8111881a2e3b3d64cb8eb9706f
Softhills Co., Ltd <gjkim@softhills.net>
2022-10-06 21:01:19 +0900

    Update AssemblyInfo.cs

--- WinForms/VIZCore3D.NET.Snapshot/Properties/AssemblyInfo.cs
+++ WinForms/VIZCore3D.NET.Snapshot/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Softhills Co., Ltd.")]
 [assembly: AssemblyProduct("VIZCore3D.NET.Snapshot")]
-[assembly: AssemblyCopyright("© 2020 Softhills Co., Ltd. All rights reserved.")]
+[assembly: AssemblyCopyright("© 2020 SOFTHILLS Co., Ltd. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
