d9be6d6de3a1d02886494932eb5a113d3f40e8ce
Gjkim <gjkim@softhills.net>
2023-05-20 12:21:11 +0900

    Update

--- WinForms/VIZCore3D.NET.ClashTest.V4/FrmMain.Designer.cs
+++ WinForms/VIZCore3D.NET.ClashTest.V4/FrmMain.Designer.cs
@@ -28,13 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ckClashEnable = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1436, 697);
+            this.splitContainer1.SplitterDistance = 352;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnOpen);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(331, 68);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Model";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(39, 29);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.ckClashEnable);
+            this.groupBox2.Location = new System.Drawing.Point(12, 86);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(331, 79);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Clash Option";
+            // 
+            // ckClashEnable
+            // 
+            this.ckClashEnable.AutoSize = true;
+            this.ckClashEnable.Location = new System.Drawing.Point(39, 34);
+            this.ckClashEnable.Name = "ckClashEnable";
+            this.ckClashEnable.Size = new System.Drawing.Size(63, 16);
+            this.ckClashEnable.TabIndex = 0;
+            this.ckClashEnable.Text = "Enable";
+            this.ckClashEnable.UseVisualStyleBackColor = true;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1436, 697);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VIZCore3D.NET.ClashTest.V4";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox ckClashEnable;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnOpen;
     }
 }
 
--- WinForms/VIZCore3D.NET.ClashTest.V4/FrmMain.cs
+++ WinForms/VIZCore3D.NET.ClashTest.V4/FrmMain.cs
@@ -11,9 +11,143 @@ namespace VIZCore3D.NET.ClashTest.V4
 {
     public partial class FrmMain : Form
     {
+        // ================================================
+        // Attribute & Property
+        // ================================================
+        /// <summary>
+        /// VIZCore3D.NET
+        /// </summary>
+        public VIZCore3D.NET.VIZCore3DControl vizcore3d { get; set; }
+
+
+        // ================================================
+        // Contruction
+        // ================================================
         public FrmMain()
         {
             InitializeComponent();
+
+            // Initialize VIZCore3D.NET
+            VIZCore3D.NET.ModuleInitializer.Run();
+
+            // Construction
+            vizcore3d = new VIZCore3D.NET.VIZCore3DControl();
+            vizcore3d.Dock = DockStyle.Fill;
+            splitContainer1.Panel2.Controls.Add(vizcore3d);
+
+            // Event
+            vizcore3d.OnInitializedVIZCore3D += VIZCore3D_OnInitializedVIZCore3D;
+        }
+
+        // ================================================
+        // Event - VIZCore3D.NET
+        // ================================================
+        #region Event - OnInitializedVIZCore3D
+        private void VIZCore3D_OnInitializedVIZCore3D(object sender, EventArgs e)
+        {
+            //MessageBox.Show("OnInitializedVIZCore3D", "VIZCore3D.NET");
+
+            // ================================================================
+            // Example
+            // ================================================================
+            //vizcore3d.License.LicenseFile("C:\\Temp\\VIZCore3D.NET.lic");                         // 라이선스 파일
+            //vizcore3d.License.LicenseServer("127.0.0.1", 8901);                                   // 라이선스 서버 - 제품 오토 선택
+            //vizcore3d.License.LicenseServer("127.0.0.1", 8901, Data.Products.AUTO);               // 라이선스 서버 - 제품 오토 선택
+            //vizcore3d.License.LicenseServer("127.0.0.1", 8901, Data.Products.VIZZARD_MANAGER);    // 라이선스 서버 - 지정된 제품으로 인증
+            //vizcore3d.License.LicenseServer("127.0.0.1", 8901, Data.Products.VIZZARD_STANDARD);   // 라이선스 서버 - 지정된 제품으로 인증
+            //vizcore3d.License.LicenseServer("127.0.0.1", 8901, Data.Products.VIZCORE3D_MANAGER);  // 라이선스 서버 - 지정된 제품으로 인증
+            //vizcore3d.License.LicenseServer("127.0.0.1", 8901, Data.Products.VIZCORE3D_STANDARD); // 라이선스 서버 - 지정된 제품으로 인증
+
+
+            // ================================================================
+            // TEST
+            // ================================================================
+            //VIZCore3D.NET.Data.LicenseResults result = vizcore3d.License.LicenseFile("C:\\License\\VIZCore3D.NET.lic");
+            //VIZCore3D.NET.Data.LicenseResults result = vizcore3d.License.LicenseServer("127.0.0.1", 8901);
+            //VIZCore3D.NET.Data.LicenseResults result = vizcore3d.License.LicenseServer("192.168.0.215", 8901);
+            //if (result != VIZCore3D.NET.Data.LicenseResults.SUCCESS)
+            //{
+            //    MessageBox.Show(string.Format("LICENSE CODE : {0}", result.ToString()), "VIZCore3D.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    return;
+            //}
+
+
+            // ================================================================
+            // License Helper
+            // ================================================================
+            #region 라이선스
+            // 라이선스 정보 조회
+            VIZCore3D.NET.Utility.LicenseHelper.LicenseData licenseData = VIZCore3D.NET.Utility.LicenseHelper.GetLicenseDataKind();
+
+            // 등록된 정보 없는 경우, 설정 다이얼로그 실행
+            if (licenseData == VIZCore3D.NET.Utility.LicenseHelper.LicenseData.NONE)
+            {
+                VIZCore3D.NET.Dialogs.LicenseDialog dlg = new VIZCore3D.NET.Dialogs.LicenseDialog();
+                if (dlg.ShowDialog() != DialogResult.OK) return;
+            }
+
+            // 라이선스 정보 재조회
+            licenseData = VIZCore3D.NET.Utility.LicenseHelper.GetLicenseDataKind();
+
+            // 등록된 정보 조회
+            Dictionary<string, string> licenseInfo = VIZCore3D.NET.Utility.LicenseHelper.GetLicenseInformation();
+            VIZCore3D.NET.Data.LicenseResults licenseResult = VIZCore3D.NET.Data.LicenseResults.NONE;
+
+            // 라이선스 서버
+            if (licenseData == VIZCore3D.NET.Utility.LicenseHelper.LicenseData.SERVER)
+            {
+                licenseResult = vizcore3d.License.LicenseServer(
+                    licenseInfo.ContainsKey("LICENSE_IP") == true ? licenseInfo["LICENSE_IP"] : String.Empty
+                    , licenseInfo.ContainsKey("LICENSE_PORT") == true ? Convert.ToInt32(licenseInfo["LICENSE_PORT"]) : 8901
+                    );
+            }
+            // 라이선스 파일
+            else if (licenseData == VIZCore3D.NET.Utility.LicenseHelper.LicenseData.FILE)
+            {
+                licenseResult = vizcore3d.License.LicenseFile(
+                    licenseInfo.ContainsKey("LICENSE_FILE") == true ? licenseInfo["LICENSE_FILE"] : String.Empty
+                    );
+            }
+
+            // 인증 결과
+            if (licenseResult != VIZCore3D.NET.Data.LicenseResults.SUCCESS)
+            {
+                MessageBox.Show(string.Format("LICENSE CODE : {0}", licenseResult.ToString()), "VIZCore3D.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            #endregion
+
+            // Init. VIZCore3D.NET
+            InitializeVIZCore3D();
+            InitializeVIZCore3DEvent();
+        }
+        #endregion
+
+        // ================================================
+        // Function - VIZCore3D.NET : Initialize
+        // ================================================
+        #region Function - VIZCore3D.NET : Initialize
+        private void InitializeVIZCore3D()
+        {
+            
+        }
+        #endregion
+
+        #region Function - VIZCore3D.NET : Event
+        /// <summary>
+        /// 이벤트 등록
+        /// </summary>
+        private void InitializeVIZCore3DEvent()
+        {
+        }
+        #endregion
+
+        // ================================================
+        // Event
+        // ================================================
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            vizcore3d.Model.OpenFileDialog();
         }
     }
 }
