2a4c2c1cd97d337875956fea92c109ce66fc59fe
Gjkim <gjkim@softhills.net>
2023-07-30 16:07:31 +0900

    Near Objects

---
+++ WinForms/VIZCore3D.NET.NearestObjects/FrmMain.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace VIZCore3D.NET.NearestObjects
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ckEnable = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lvNodes = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ckVisibleOnly = new System.Windows.Forms.CheckBox();
+            this.ckOpaqueOnly = new System.Windows.Forms.CheckBox();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1173, 521);
+            this.splitContainer1.SplitterDistance = 392;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.ckOpaqueOnly);
+            this.groupBox1.Controls.Add(this.ckVisibleOnly);
+            this.groupBox1.Controls.Add(this.ckEnable);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(371, 97);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
+            // 
+            // ckEnable
+            // 
+            this.ckEnable.AutoSize = true;
+            this.ckEnable.Location = new System.Drawing.Point(21, 24);
+            this.ckEnable.Name = "ckEnable";
+            this.ckEnable.Size = new System.Drawing.Size(63, 16);
+            this.ckEnable.TabIndex = 0;
+            this.ckEnable.Text = "Enable";
+            this.ckEnable.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.lvNodes);
+            this.groupBox2.Location = new System.Drawing.Point(12, 115);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(371, 308);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Nodes";
+            // 
+            // lvNodes
+            // 
+            this.lvNodes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvNodes.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lvNodes.FullRowSelect = true;
+            this.lvNodes.GridLines = true;
+            this.lvNodes.HideSelection = false;
+            this.lvNodes.Location = new System.Drawing.Point(6, 20);
+            this.lvNodes.Name = "lvNodes";
+            this.lvNodes.Size = new System.Drawing.Size(359, 282);
+            this.lvNodes.TabIndex = 0;
+            this.lvNodes.UseCompatibleStateImageBehavior = false;
+            this.lvNodes.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Index";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            this.columnHeader2.Width = 120;
+            // 
+            // ckVisibleOnly
+            // 
+            this.ckVisibleOnly.AutoSize = true;
+            this.ckVisibleOnly.Checked = true;
+            this.ckVisibleOnly.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ckVisibleOnly.Location = new System.Drawing.Point(21, 46);
+            this.ckVisibleOnly.Name = "ckVisibleOnly";
+            this.ckVisibleOnly.Size = new System.Drawing.Size(92, 16);
+            this.ckVisibleOnly.TabIndex = 1;
+            this.ckVisibleOnly.Text = "Visible Only";
+            this.ckVisibleOnly.UseVisualStyleBackColor = true;
+            // 
+            // ckOpaqueOnly
+            // 
+            this.ckOpaqueOnly.AutoSize = true;
+            this.ckOpaqueOnly.Checked = true;
+            this.ckOpaqueOnly.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ckOpaqueOnly.Location = new System.Drawing.Point(21, 68);
+            this.ckOpaqueOnly.Name = "ckOpaqueOnly";
+            this.ckOpaqueOnly.Size = new System.Drawing.Size(98, 16);
+            this.ckOpaqueOnly.TabIndex = 2;
+            this.ckOpaqueOnly.Text = "Opaque Only";
+            this.ckOpaqueOnly.UseVisualStyleBackColor = true;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Distance";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader3.Width = 79;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Point";
+            this.columnHeader4.Width = 75;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1173, 521);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nearest Objects";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox ckEnable;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView lvNodes;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.CheckBox ckOpaqueOnly;
+        private System.Windows.Forms.CheckBox ckVisibleOnly;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
