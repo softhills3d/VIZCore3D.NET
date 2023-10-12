f18f61cd3bc850e0f3b7c059384e78b698b40312
Gjkim <gjkim@softhills.net>
2023-10-12 23:26:54 +0900

    Add RegisterProtocolHandler

---
+++ Utility/RegisterProtocolHandler/FrmMain.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace RegisterProtocolHandler
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAppName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnOpen);
+            this.groupBox1.Controls.Add(this.txtPath);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtAppName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(613, 103);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Scheme";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "App. Unique Name";
+            // 
+            // txtAppName
+            // 
+            this.txtAppName.Location = new System.Drawing.Point(160, 29);
+            this.txtAppName.Name = "txtAppName";
+            this.txtAppName.Size = new System.Drawing.Size(100, 21);
+            this.txtAppName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Application";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(160, 62);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(353, 21);
+            this.txtPath.TabIndex = 3;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpen.Location = new System.Drawing.Point(519, 62);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "Select";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRegister.Location = new System.Drawing.Point(531, 121);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(75, 23);
+            this.btnRegister.TabIndex = 5;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 156);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registering an Application to a URI Scheme";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAppName;
+        private System.Windows.Forms.Button btnRegister;
+    }
+}
+
---
+++ Utility/RegisterProtocolHandler/FrmMain.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RegisterProtocolHandler
+{
+    public partial class FrmMain : Form
+    {
+        public string Key_AppUniqueName = "app";
+        public const string Key_URLProtocol = "URL Protocol";
+        public const string Key_DefaultIcon = "DefaultIcon";
+        public const string Key_Shell = "shell";
+        public const string Key_Open = "open";
+        public const string Key_Command = "command";
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "EXE (*.exe)|*.exe";
+
+            if (dlg.ShowDialog() != DialogResult.OK) return;
+
+            txtPath.Text = dlg.FileName;
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            string app = txtAppName.Text;
+            string path = txtPath.Text;
+
+            if (String.IsNullOrEmpty(app) == true) return;
+            if (String.IsNullOrEmpty(path) == true) return;
+
+            Key_AppUniqueName = app;
+
+            RegisterProtocolHandler(path);
+        }
+
+        #region Registering an Application to a URI Scheme
+        /// <summary>
+        /// Registering an Application to a URI Scheme
+        /// </summary>
+        /// <param name="doRegistration"></param>
+        private void RegisterProtocolHandler(string path)
+        {
+            if (String.IsNullOrEmpty(path) == true)
+                throw new NullReferenceException("Path is null.");
+
+            if (System.IO.File.Exists(path) == false)
+                throw new System.IO.FileNotFoundException(path);
+
+            try
+            {
+                string exeName = System.IO.Path.GetFileName(path);
+
+                using (Microsoft.Win32.RegistryKey appKey = Microsoft.Win32.Registry.ClassesRoot.CreateSubKey(Key_AppUniqueName))
+                {
+                    appKey.SetValue(null, string.Format("URL:Run {0} Protocol", Key_AppUniqueName.ToUpper()));
+                    appKey.SetValue(Key_URLProtocol, "");
+
+                    using (Microsoft.Win32.RegistryKey defaultIconKey = appKey.CreateSubKey(Key_DefaultIcon))
+                    {
+                        defaultIconKey.SetValue(null, string.Format("{0},1", exeName));
+                    }
+
+                    using (Microsoft.Win32.RegistryKey shellKey = appKey.CreateSubKey(Key_Shell))
+                    using (Microsoft.Win32.RegistryKey openKey = shellKey.CreateSubKey(Key_Open))
+                    using (Microsoft.Win32.RegistryKey commandKey = openKey.CreateSubKey(Key_Command))
+                    {
+                        string value = string.Format("\"{0}\" \"%1\"", path);
+                        commandKey.SetValue(null, value);
+                    }
+                }
+
+                MessageBox.Show("Completed.", "RegisterProtocolHandler", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        #endregion
+    }
+}
