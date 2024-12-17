108b5072c67c49b8a334556c352902c7b761823f
gjkim <gjkim@softhills.net>
2024-12-17 13:03:54 +0900

    Material Ex

---
+++ WinForms/VIZCore3D.NET.Material/FrmMain.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace VIZCore3D.NET.Material
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lvItems = new System.Windows.Forms.ListView();
+            this.imgThumb = new System.Windows.Forms.ImageList(this.components);
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSelectFiles = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtObjectIndex = new System.Windows.Forms.TextBox();
+            this.btnApply = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1170, 690);
+            this.splitContainer1.SplitterDistance = 390;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnSelectFiles);
+            this.groupBox1.Controls.Add(this.btnLoad);
+            this.groupBox1.Controls.Add(this.lvItems);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(371, 411);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Materials";
+            // 
+            // lvItems
+            // 
+            this.lvItems.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvItems.HideSelection = false;
+            this.lvItems.LargeImageList = this.imgThumb;
+            this.lvItems.Location = new System.Drawing.Point(17, 72);
+            this.lvItems.Name = "lvItems";
+            this.lvItems.Size = new System.Drawing.Size(337, 319);
+            this.lvItems.TabIndex = 0;
+            this.lvItems.UseCompatibleStateImageBehavior = false;
+            // 
+            // imgThumb
+            // 
+            this.imgThumb.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imgThumb.ImageSize = new System.Drawing.Size(64, 64);
+            this.imgThumb.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(17, 34);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSelectFiles
+            // 
+            this.btnSelectFiles.Location = new System.Drawing.Point(109, 34);
+            this.btnSelectFiles.Name = "btnSelectFiles";
+            this.btnSelectFiles.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFiles.TabIndex = 2;
+            this.btnSelectFiles.Text = "Select";
+            this.btnSelectFiles.UseVisualStyleBackColor = true;
+            this.btnSelectFiles.Click += new System.EventHandler(this.btnSelectFiles_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.btnApply);
+            this.groupBox2.Controls.Add(this.txtObjectIndex);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 429);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(371, 77);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Object";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Object Index";
+            // 
+            // txtObjectIndex
+            // 
+            this.txtObjectIndex.Location = new System.Drawing.Point(97, 32);
+            this.txtObjectIndex.Name = "txtObjectIndex";
+            this.txtObjectIndex.ReadOnly = true;
+            this.txtObjectIndex.Size = new System.Drawing.Size(100, 21);
+            this.txtObjectIndex.TabIndex = 1;
+            // 
+            // btnApply
+            // 
+            this.btnApply.Location = new System.Drawing.Point(219, 32);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(75, 23);
+            this.btnApply.TabIndex = 2;
+            this.btnApply.Text = "Apply";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1170, 690);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VIZCore3D.NET.Material";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView lvItems;
+        private System.Windows.Forms.Button btnSelectFiles;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ImageList imgThumb;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.TextBox txtObjectIndex;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WinForms/VIZCore3D.NET.Material/FrmMain.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace VIZCore3D.NET.Material
+{
+    public partial class FrmMain : Form
+    {
+        private VIZCore3D.NET.VIZCore3DControl vizcore3d { get; set; }
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            VIZCore3D.NET.ModuleInitializer.Run();
+
+            vizcore3d = new VIZCore3DControl();
+            vizcore3d.Dock = DockStyle.Fill;
+
+            splitContainer1.Panel2.Controls.Add(vizcore3d);
+
+            vizcore3d.OnInitializedVIZCore3D += VIZCore3D_OnInitializedVIZCore3D;
+        }
+
+        private void VIZCore3D_OnInitializedVIZCore3D(object sender, EventArgs e)
+        {
+            vizcore3d.License.LicenseServer("127.0.0.1", 8901);
+
+            vizcore3d.Object3D.OnObject3DSelected += Object3D_OnObject3DSelected;
+        }
+
+        private void Object3D_OnObject3DSelected(object sender, Event.EventManager.Object3DSelectedEventArgs e)
+        {
+            if (e.Node.Count == 0)
+            {
+                txtObjectIndex.Text = String.Empty;
+                return;
+            }
+
+            txtObjectIndex.Text = e.Node[0].Index.ToString();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            string app = Application.StartupPath;
+            string res = string.Format("{0}\\Resources", app);
+            string[] files = System.IO.Directory.GetFiles(res);
+
+            AddImageItem(files);
+        }
+
+        private void btnSelectFiles_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Multiselect = true;
+            dlg.Filter = "PNG (*.png)|*.png";
+            if (dlg.ShowDialog() != DialogResult.OK) return;
+
+            AddImageItem(dlg.FileNames);
+        }
+
+        private void AddImageItem(string[] items)
+        {
+            lvItems.Items.Clear();
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                try
+                {
+                    var image = Image.FromFile(items[i]);
+                    imgThumb.Images.Add(image);
+
+                    var item = new ListViewItem
+                    {
+                        Text = System.IO.Path.GetFileNameWithoutExtension(items[i]),
+                        ImageIndex = i
+                    };
+
+                    lvItems.Items.Add(item);
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            string index = txtObjectIndex.Text;
+            if (String.IsNullOrEmpty(index) == true) return;
+
+            // Continue....
+        }
+    }
+}
