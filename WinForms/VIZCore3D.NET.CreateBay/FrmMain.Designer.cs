e0fa60209ba1731d76d2f593a99f5bdb3aebea25
bukoo <bukoo@softhills.net>
2025-02-10 15:33:56 +0900

    베이 생성 예제 추가

---
+++ WinForms/VIZCore3D.NET.CreateBay/FrmMain.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace VIZCore3D.NET.PlacementStatus
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnMake = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtBaySpacing = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtContentsRow = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtContentsColumn = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBayHeight = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtBayWidth = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnMake);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1145, 714);
+            this.splitContainer1.SplitterDistance = 381;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // btnMake
+            // 
+            this.btnMake.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMake.Location = new System.Drawing.Point(292, 229);
+            this.btnMake.Name = "btnMake";
+            this.btnMake.Size = new System.Drawing.Size(75, 23);
+            this.btnMake.TabIndex = 1;
+            this.btnMake.Text = "Make";
+            this.btnMake.UseVisualStyleBackColor = true;
+            this.btnMake.Click += new System.EventHandler(this.btnMake_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.txtBayHeight);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtBayWidth);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtBaySpacing);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtContentsRow);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtContentsColumn);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(355, 211);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Property";
+            // 
+            // txtBaySpacing
+            // 
+            this.txtBaySpacing.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBaySpacing.Location = new System.Drawing.Point(138, 104);
+            this.txtBaySpacing.Name = "txtBaySpacing";
+            this.txtBaySpacing.Size = new System.Drawing.Size(190, 21);
+            this.txtBaySpacing.TabIndex = 5;
+            this.txtBaySpacing.Text = "1000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Bay Spacing";
+            // 
+            // txtContentsRow
+            // 
+            this.txtContentsRow.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtContentsRow.Location = new System.Drawing.Point(138, 63);
+            this.txtContentsRow.Name = "txtContentsRow";
+            this.txtContentsRow.Size = new System.Drawing.Size(190, 21);
+            this.txtContentsRow.TabIndex = 3;
+            this.txtContentsRow.Text = "4";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Row";
+            // 
+            // txtContentsColumn
+            // 
+            this.txtContentsColumn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtContentsColumn.Location = new System.Drawing.Point(138, 36);
+            this.txtContentsColumn.Name = "txtContentsColumn";
+            this.txtContentsColumn.Size = new System.Drawing.Size(190, 21);
+            this.txtContentsColumn.TabIndex = 1;
+            this.txtContentsColumn.Text = "4";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Column";
+            // 
+            // txtBayHeight
+            // 
+            this.txtBayHeight.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBayHeight.Location = new System.Drawing.Point(138, 159);
+            this.txtBayHeight.Name = "txtBayHeight";
+            this.txtBayHeight.Size = new System.Drawing.Size(190, 21);
+            this.txtBayHeight.TabIndex = 15;
+            this.txtBayHeight.Text = "15000";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 162);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 12);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Bay Height";
+            // 
+            // txtBayWidth
+            // 
+            this.txtBayWidth.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBayWidth.Location = new System.Drawing.Point(138, 132);
+            this.txtBayWidth.Name = "txtBayWidth";
+            this.txtBayWidth.Size = new System.Drawing.Size(190, 21);
+            this.txtBayWidth.TabIndex = 13;
+            this.txtBayWidth.Text = "15000";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Bay Width";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 714);
+            this.Controls.Add(this.splitContainer1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VIZCore3D.NET.CreateBay";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtContentsColumn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtContentsRow;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBaySpacing;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnMake;
+        private System.Windows.Forms.TextBox txtBayHeight;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtBayWidth;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WinForms/VIZCore3D.NET.CreateBay/FrmMain.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VIZCore3D.NET.PlacementStatus
+{
+    public partial class FrmMain : Form
+    {
+        /// <summary>
+        /// VIZCore3D.NET
+        /// </summary>
+        private VIZCore3D.NET.VIZCore3DControl vizcore3d;
+
+        /// <summary>
+        /// 영역 저장 [ Bay Title 입력 하기 위함 ]
+        /// </summary>
+        private Dictionary<string, Data.BoundBox3D> SpaceManager { get; set; }
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            // Initialize VIZCore3D.NET
+            VIZCore3D.NET.ModuleInitializer.Run();
+
+            // Construction
+            vizcore3d = new VIZCore3D.NET.VIZCore3DControl();
+            vizcore3d.Dock = DockStyle.Fill;
+            splitContainer1.Panel2.Controls.Add(vizcore3d);
+
+            // Event
+            vizcore3d.OnInitializedVIZCore3D += VIZCore3D_OnInitializedVIZCore3D;
+
+            SpaceManager = new Dictionary<string, Data.BoundBox3D>();
+        }
+
+        private void VIZCore3D_OnInitializedVIZCore3D(object sender, EventArgs e)
+        {
+            // 라이선스 서버 주소 입력
+            vizcore3d.License.LicenseServer("127.0.0.1", 8901);
+        } 
+
+        private void MakeDashboard()
+        {
+            // 베이를 생성 할  행 / 열 수치
+            float contentsColumn = Convert.ToSingle(txtContentsColumn.Text); // 4.0f;
+            float contentsRow = Convert.ToSingle(txtContentsRow.Text); // 5.0f;
+
+            // 베이 생성 간격 ( 행 / 열 )
+            float spacing = Convert.ToSingle(txtBaySpacing.Text); // 1000.0f;
+
+            // 베이 크기
+            float bayWidth = Convert.ToSingle(txtBayWidth.Text); // 15000.0f;
+            float bayHeight = Convert.ToSingle(txtBayHeight.Text); // 15000.0f;
+
+            vizcore3d.Model.Close();
+            SpaceManager.Clear();
+
+            // 루트 노드 생성
+            VIZCore3D.NET.Manager.PrimitiveObject root = vizcore3d.Primitive.AddNode("Bays");
+
+            // 행 / 열 기준으로 생성
+            int ID = 1;
+            for (int i = 0; i < contentsRow; i++)
+            {
+                for (int j = 0; j < contentsColumn; j++)
+                {
+                    {
+                        VIZCore3D.NET.Manager.PrimitiveObject modelItem = root.AddNode(string.Format("Bay_{0:D2}", ID), 10);
+
+                        // Primitive Box 생성
+                        VIZCore3D.NET.Manager.PrimitiveBox bayBox = new VIZCore3D.NET.Manager.PrimitiveBox();
+
+                        // 생성 위치 지정 [ 간격 설정 ]
+                        VIZCore3D.NET.Data.Vertex3D min = new VIZCore3D.NET.Data.Vertex3D(
+                            j * (bayWidth + spacing)
+                            , -i * (bayHeight + spacing)
+                            , -500
+                            );
+
+                        VIZCore3D.NET.Data.Vertex3D max = new VIZCore3D.NET.Data.Vertex3D(
+                            j * (bayWidth + spacing) + bayWidth
+                            , -i * (bayHeight + spacing) - bayHeight
+                            , 0
+                            );
+
+                        // 영역 저장
+                        VIZCore3D.NET.Data.BoundBox3D bayBoundBox = new VIZCore3D.NET.Data.BoundBox3D(min, max);
+                        SpaceManager.Add(string.Format("Bay_{0:D2}", ID), bayBoundBox);
+                        bayBox.SetMinMaxPoints(bayBoundBox);
+                        modelItem.AddPrimitive(bayBox);
+                    }
+
+                    ID++;
+                }
+            }
+            vizcore3d.TextDrawing.Clear();
+
+            // 글꼴 설정 값
+            int fontHeight = 1000;
+            float fontSize = 100; 
+
+            // 각 Bay 마다 Title 추가 [ 영역 저장된 이름을 표기 ] ex) Bay_01..Bay_02..Bay_03..
+            foreach (KeyValuePair<string, Data.BoundBox3D> item in SpaceManager)
+            {
+                if (item.Key.Contains("Bay_") == false) continue;
+
+                VIZCore3D.NET.Data.Vertex3D center = new VIZCore3D.NET.Data.Vertex3D();
+                center.X = item.Value.MinX;
+                center.Y = item.Value.MinY + item.Value.LengthY;
+                center.Z = item.Value.MaxZ;
+
+                vizcore3d.TextDrawing.Add(
+                    center
+                    , new VIZCore3D.NET.Data.Vector3D(1, 0, 0) /* X+ 방향으로 텍스트가 표시 */
+                    , new VIZCore3D.NET.Data.Vector3D(0, 1, 0) /* 텍스트의 위쪽 방향 : Z+에서 볼때, Y+가 위쪽 임. */
+                    , fontHeight
+                    , fontSize
+                    , Color.Black
+                    , item.Key
+                    );
+            }
+            
+            // Primitive를 생성 하기 위한 공백 모델 생성 [ "BAY_BLOCK" 명칭 지정 ]
+            vizcore3d.Primitive.OpenModel("BAY_BLOCK");
+
+            // 화면 설정 변경
+            vizcore3d.View.MoveCamera(VIZCore3D.NET.Data.CameraDirection.Z_PLUS);
+            vizcore3d.View.Navigation = VIZCore3D.NET.Data.NavigationModes.PAN;
+            vizcore3d.View.SetRenderMode(VIZCore3D.NET.Data.RenderModes.SMOOTH_EDGE);
+            vizcore3d.View.FitToView();
+        }
+        private void btnMake_Click(object sender, EventArgs e)
+        {
+            MakeDashboard();
+            vizcore3d.View.SilhouetteEdge = true;
+        }
+    }
+}
