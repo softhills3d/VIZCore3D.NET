818953d3ef1f0e56868e316fd32d5998575e4b66
bukoo <bukoo@softhills.net>
2025-05-23 14:30:54 +0900

    API 출처 주소 추가

--- WinForms/VIZCore3D.NET.Osnap2DPoint/Form1.cs
+++ WinForms/VIZCore3D.NET.Osnap2DPoint/Form1.cs
@@ -43,6 +43,12 @@ namespace VIZCore3D.NET.Osnap2DPoint
             vizcore3d.GeometryUtility.OnOsnapPickingItem += GeometryUtility_OnOsnapPickingItem;
         }
 
+        /// <summary>
+        /// 선택한 Onsap 측정 포인트를 가져옵니다.
+        /// https://softhills.net/SHDC/VIZCore3D.NET/Help/html/E_VIZCore3D_NET_Manager_GeometryUtilityManager_OnOsnapPickingItem.htm
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void GeometryUtility_OnOsnapPickingItem(object sender, EventManager.OsnapPickingItemEventArgs e)
         {
             // 화면 기준 2D 포인트 좌표 변경
@@ -54,21 +60,27 @@ namespace VIZCore3D.NET.Osnap2DPoint
 
         private void btnShowOsnap_Click(object sender, EventArgs e)
         {
-            // 카메라 회전 및 축 고정
+            // 카메라 회전
+            // https://softhills.net/SHDC/VIZCore3D.NET/Help/html/M_VIZCore3D_NET_Manager_ViewManager_MoveCamera_2.htm
             vizcore3d.View.MoveCamera(VIZCore3D.NET.Data.CameraDirection.Z_PLUS);
 
             // 카메라 회전 고정
+            // https://softhills.net/SHDC/VIZCore3D.NET/Help/html/P_VIZCore3D_NET_Manager_ScreenAxisRotationManager_LockZAxis.htm
             vizcore3d.View.ScreenAxisRotation.LockZAxis = true;
 
             // 화면 이동 모드 변경
+            // https://softhills.net/SHDC/VIZCore3D.NET/Help/html/P_VIZCore3D_NET_Manager_ViewManager_Navigation.htm
             vizcore3d.View.Navigation = Data.NavigationModes.PAN;
 
             // 선택된 개체 가져오기
+            // https://softhills.net/SHDC/VIZCore3D.NET/Help/html/M_VIZCore3D_NET_Manager_Object3DManager_FromFilter.htm
             List<VIZCore3D.NET.Data.Node> items = vizcore3d.Object3D.FromFilter(Data.Object3dFilter.SELECTED_TOP);
 
             // 개체 선택 방식
             if (items.Count == 0)
             {
+                // 개체 선택 Osnap 표시
+                // https://softhills.net/SHDC/VIZCore3D.NET/Help/html/M_VIZCore3D_NET_Manager_GeometryUtilityManager_ShowOsnap.htm
                 vizcore3d.GeometryUtility.ShowOsnap(
                     false   // 면 표시 여부
                     , true  // 점 표시 여부
@@ -79,6 +91,8 @@ namespace VIZCore3D.NET.Osnap2DPoint
             // 개체 지정 방식
             else
             {
+                // 개체 지정 Osnap 표시
+                // https://softhills.net/SHDC/VIZCore3D.NET/Help/html/M_VIZCore3D_NET_Manager_GeometryUtilityManager_ShowOsnap_2.htm
                 vizcore3d.GeometryUtility.ShowOsnap(
                     items[0].Index  // 개체 인덱스
                     , false         // 면 표시 여부
