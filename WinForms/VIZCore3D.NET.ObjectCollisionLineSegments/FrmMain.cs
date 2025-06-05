b6e6419745a6e2523bb259f6d3bec9a05f2113ed
gjkim <gjkim@softhills.net>
2025-06-05 23:41:37 +0800

    VIZCore3D.NET.ObjectCollisionLineSegments

---
+++ WinForms/VIZCore3D.NET.ObjectCollisionLineSegments/FrmMain.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VIZCore3D.NET.ObjectCollisionLineSegments
+{
+    public partial class FrmMain : Form
+    {
+        private VIZCore3D.NET.VIZCore3DControl vizcore3d { get; set; }
+        private List<VIZCore3D.NET.Data.Node> SourceNodes { get; set; }
+        private List<VIZCore3D.NET.Data.Node> TargetNodes { get; set; }
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            VIZCore3D.NET.ModuleInitializer.Run();
+
+            vizcore3d = new VIZCore3DControl();
+            vizcore3d.Dock = DockStyle.Fill;
+            vizcore3d.OnInitializedVIZCore3D += VIZCore3D_OnInitializedVIZCore3D;
+            this.splitContainer1.Panel2.Controls.Add(vizcore3d);
+
+            btnColor.BackColor = Color.Red;
+        }
+
+        private void VIZCore3D_OnInitializedVIZCore3D(object sender, EventArgs e)
+        {
+            VIZCore3D.NET.Data.LicenseResults results 
+                = vizcore3d.License.LicenseServer("127.0.0.1", 8901);
+
+            if(results != VIZCore3D.NET.Data.LicenseResults.SUCCESS)
+            {
+                MessageBox.Show("License Error : " + results.ToString());
+                return;
+            }
+        }
+
+        private void btnGetSource_Click(object sender, EventArgs e)
+        {
+            if (vizcore3d.Model.IsOpen() == false) return;
+
+            SourceNodes
+                = vizcore3d.Object3D.FromFilter(VIZCore3D.NET.Data.Object3dFilter.SELECTED_PART);
+
+            if (SourceNodes.Count == 0) return;
+
+            txtSource.Text = SourceNodes[0].NodeName;
+        }
+
+        private void btnGetTarget_Click(object sender, EventArgs e)
+        {
+            if (vizcore3d.Model.IsOpen() == false) return;
+
+            TargetNodes
+                = vizcore3d.Object3D.FromFilter(VIZCore3D.NET.Data.Object3dFilter.SELECTED_PART);
+
+            if (TargetNodes.Count == 0) return;
+
+            txtTarget.Text = TargetNodes[0].NodeName;
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            if (vizcore3d.Model.IsOpen() == false) return;
+            if (SourceNodes == null || SourceNodes.Count == 0) return;
+            
+            // Clear
+            vizcore3d.Object3D.Select(VIZCore3D.NET.Data.Object3dSelectionModes.DESELECT_ALL);
+            lvItems.Items.Clear();
+            vizcore3d.ShapeDrawing.Clear();
+
+            List<VIZCore3D.NET.Data.Vector3D> lineSegments = null;
+
+            if (TargetNodes == null || TargetNodes.Count == 0)
+            {
+                lineSegments 
+                    = vizcore3d.GeometryUtility.GetObjectCollisionLine(SourceNodes);
+            }
+            else
+            {
+                lineSegments
+                    = vizcore3d.GeometryUtility.GetObjectCollisionLine(SourceNodes[0].Index, TargetNodes[0].Index);
+            }
+
+            List<VIZCore3D.NET.Data.Vertex3DItemCollection> items = new List<Data.Vertex3DItemCollection>();
+
+            lvItems.BeginUpdate();
+
+            int index = 1;
+            int groupId = 1;
+            for (int i = 0; i < lineSegments.Count - 1; i += 2)
+            {
+                Data.Vertex3DItemCollection item = new Data.Vertex3DItemCollection();
+                item.BaseCollection.Add(lineSegments[i + 0].ToVertex3D());
+                item.BaseCollection.Add(lineSegments[i + 1].ToVertex3D());
+                items.Add(item);
+
+                ListViewItem lvi1 = new ListViewItem(
+                    new string[] 
+                        {
+                            index.ToString()
+                            , groupId.ToString()
+                            , lineSegments[i + 0].X.ToString()
+                            , lineSegments[i + 0].Y.ToString()
+                            , lineSegments[i + 0].Z.ToString()
+                        }
+                    );
+
+                lvItems.Items.Add(lvi1);
+
+                index++;
+
+                ListViewItem lvi2 = new ListViewItem(
+                    new string[]
+                        {
+                            index.ToString()
+                            , groupId.ToString()
+                            , lineSegments[i + 1].X.ToString()
+                            , lineSegments[i + 1].Y.ToString()
+                            , lineSegments[i + 1].Z.ToString()
+                        }
+                    );
+
+                lvItems.Items.Add(lvi2);
+
+                index++;
+                groupId++;
+            }
+
+            lvItems.EndUpdate();
+
+            // Draw
+            int shapeId = vizcore3d.ShapeDrawing.AddLine(
+                items
+                , 0
+                , btnColor.BackColor
+                , Convert.ToSingle(txtThickness.Text)
+                , true
+            );
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            if(SourceNodes != null) SourceNodes.Clear();
+            if(TargetNodes != null) TargetNodes.Clear();
+            lvItems.Items.Clear();
+            vizcore3d.ShapeDrawing.Clear();
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog dlg = new ColorDialog();
+            dlg.AllowFullOpen = true;
+            dlg.ShowHelp = true;
+            dlg.Color = btnColor.BackColor;
+
+            if (dlg.ShowDialog() != DialogResult.OK) return;
+
+            btnColor.BackColor = dlg.Color;
+        }
+    }
+}
