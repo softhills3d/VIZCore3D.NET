f22f8d260e6f19e96e78730425e06e2550aae7e9
bukoo <bukoo@softhills.net>
2025-06-05 09:22:23 +0900

    리뷰 복사 예제 추가

---
+++ WinForms/VIZCore3D.NET.ReviewCopy/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace VIZCore3D.NET.ReviewCopy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1059, 704);
+            this.splitContainer1.SplitterDistance = 353;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Type});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 74);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(327, 618);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
+            // Type
+            // 
+            this.Type.HeaderText = "Type";
+            this.Type.Name = "Type";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(327, 56);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Review";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "복사";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1059, 704);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "VIZCore3D.NET.ReviewCopy";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Type;
+    }
+}
+
---
+++ WinForms/VIZCore3D.NET.ReviewCopy/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using VIZCore3D.NET.Event;
+
+namespace VIZCore3D.NET.ReviewCopy
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// VIZCore3D.NET
+        /// </summary>
+        private VIZCore3D.NET.VIZCore3DControl vizcore3d;
+
+        private int _slectedReviewID = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Initialize VIZCore3D.NET
+            VIZCore3D.NET.ModuleInitializer.Run();
+
+            // Construction
+            vizcore3d = new VIZCore3D.NET.VIZCore3DControl();
+            vizcore3d.Dock = DockStyle.Fill;
+            splitContainer1.Panel2.Controls.Add(vizcore3d);
+
+            // Event
+            vizcore3d.OnInitializedVIZCore3D += VIZCore3D_OnInitializedVIZCore3D;
+
+            dataGridView1.ReadOnly = true;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void VIZCore3D_OnInitializedVIZCore3D(object sender, EventArgs e)
+        {
+            // 라이선스 서버 주소 입력
+            vizcore3d.License.LicenseServer("127.0.0.1", 8901);
+
+            vizcore3d.Review.OnReviewChangedEvent += Review_OnReviewChangedEvent;
+            vizcore3d.GeometryUtility.OnOsnapPickingItem += GeometryUtility_OnOsnapPickingItem;
+        }
+
+        /// <summary>
+        /// Osnap 피킹 포인트 선택 이벤트
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GeometryUtility_OnOsnapPickingItem(object sender, EventManager.OsnapPickingItemEventArgs e)
+        {
+            var reviewItem = vizcore3d.Review.GetItem(_slectedReviewID);
+
+            // 리뷰 타입에 따라 리뷰 생성 하는 방식 다르게 [ 필요에 따라 아래 API 주소를 참고 하여 추가 ]
+            // 리뷰 타입은 https://softhills.net/SHDC/VIZCore3D.NET/Help/html/T_VIZCore3D_NET_Manager_ReviewManager_ReviewKind.htm 참고
+            switch (reviewItem.Kind)
+            {
+                case Manager.ReviewManager.ReviewKind.RK_SURFACE_NOTE:
+                    vizcore3d.Review.Note.AddNoteSurface(reviewItem.Title, e.Point.X, e.Point.Y, e.Point.Z);
+                    break;
+                case Manager.ReviewManager.ReviewKind.RK_2D_NOTE:
+                    vizcore3d.Review.Note.AddNote2D(reviewItem.Title, e.Point.X, e.Point.Y, e.Point.Z);
+                    break;
+                case Manager.ReviewManager.ReviewKind.RK_3D_NOTE:
+                    vizcore3d.Review.Note.AddNote3D(reviewItem.Title, e.Point.X, e.Point.Y, e.Point.Z);
+                    break;
+                default:
+                    break;
+            }
+
+            vizcore3d.View.Message.Clear();
+        }
+
+        /// <summary>
+        /// 리뷰 변경 이벤트
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Review_OnReviewChangedEvent(object sender, EventManager.ReviewEventArgs e)
+        {
+            if (e.EventKind == Manager.ReviewManager.ReviewEventKind.CHANGED_REVIEW_DATA)
+            {
+                // 리뷰가 추가 되거나 변경 되는 경우
+                var revieItem = vizcore3d.Review.GetItem(e.ReviewID);
+                var type = revieItem.Kind.ToString();
+
+                if (type.Contains("NOTE"))
+                {
+                    type = "Note";
+                }
+                else
+                {
+                    type = "Unknown Type";
+                }
+
+                // 타입 분류 후 데이터 추가
+                dataGridView1.Rows.Add($"{e.ReviewID}", type);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 노트 복사
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var selectedRow = dataGridView1.SelectedRows[0];
+                _slectedReviewID = int.Parse(selectedRow.Cells["ID"].Value.ToString());
+
+                // 개체 선택 시 피킹 포인트 선택 이벤트
+                vizcore3d.GeometryUtility.ShowOsnap(true, true, true, true);
+            }
+            else
+            {
+                MessageBox.Show("복사할 리뷰를 선택하세요.", "VIZCore3D.NET");
+            }
+        }
+    }
+}
