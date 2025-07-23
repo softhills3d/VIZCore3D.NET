9d46e318cb3037c09d4c4d5dacac08b15b84c111
gjkim <gjkim@softhills.net>
2025-07-23 17:51:27 +0900

    참조축 관련 API 추가

---
+++ WinForms/VIZCore3D.NET.ReferenceAxis/FrmMain.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace VIZCore3D.NET.ReferenceAxis
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Size = new System.Drawing.Size(1135, 626);
+            this.splitContainer1.SplitterDistance = 290;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1135, 626);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VIZCore3D.NET.ReferenceAxis";
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
---
+++ WinForms/VIZCore3D.NET.ReferenceAxis/FrmMain.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VIZCore3D.NET.ReferenceAxis
+{
+    public partial class FrmMain : Form
+    {
+        private VIZCore3D.NET.VIZCore3DControl vizcore3d { get; set; }
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            VIZCore3D.NET.ModuleInitializer.Run();
+
+            vizcore3d = new VIZCore3D.NET.VIZCore3DControl();
+            vizcore3d.Dock = DockStyle.Fill;
+            this.splitContainer1.Panel2.Controls.Add(vizcore3d);
+
+            vizcore3d.OnInitializedVIZCore3D += VIZCore3D_OnInitializedVIZCore3D;
+        }
+
+        private void VIZCore3D_OnInitializedVIZCore3D(object sender, EventArgs e)
+        {
+            VIZCore3D.NET.Data.LicenseResults result
+                = vizcore3d.License.LicenseServer("127.0.0.1", 8901);
+
+            if(result != VIZCore3D.NET.Data.LicenseResults.SUCCESS)
+            {
+                return;
+            }
+        }
+    }
+}
