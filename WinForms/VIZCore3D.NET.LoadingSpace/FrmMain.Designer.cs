72ad7130ffb44895d4891d8ef613e157362af4d4
gjkim <gjkim@softhills.net>
2025-09-07 17:42:43 +0900

    심벌 기능 (구현중/배포금지)

---
+++ WinForms/VIZCore3D.NET.LoadingSpace/FrmMain.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace VIZCore3D.NET.LoadingSpace
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtBlock = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAnimatePath = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1005, 525);
+            this.splitContainer1.SplitterDistance = 335;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtBlock);
+            this.groupBox1.Controls.Add(this.btnOpen);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(312, 64);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Env.";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(22, 24);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Model";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.btnAnimatePath);
+            this.groupBox2.Location = new System.Drawing.Point(12, 82);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(312, 64);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Path";
+            // 
+            // txtBlock
+            // 
+            this.txtBlock.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtBlock.Location = new System.Drawing.Point(154, 26);
+            this.txtBlock.Name = "txtBlock";
+            this.txtBlock.Size = new System.Drawing.Size(100, 21);
+            this.txtBlock.TabIndex = 1;
+            this.txtBlock.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtBlock_KeyUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Block";
+            // 
+            // btnAnimatePath
+            // 
+            this.btnAnimatePath.Location = new System.Drawing.Point(22, 22);
+            this.btnAnimatePath.Name = "btnAnimatePath";
+            this.btnAnimatePath.Size = new System.Drawing.Size(75, 23);
+            this.btnAnimatePath.TabIndex = 3;
+            this.btnAnimatePath.Text = "Animate";
+            this.btnAnimatePath.UseVisualStyleBackColor = true;
+            this.btnAnimatePath.Click += new System.EventHandler(this.btnAnimatePath_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 525);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Loading Space";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBlock;
+        private System.Windows.Forms.Button btnAnimatePath;
+    }
+}
+
---
+++ WinForms/VIZCore3D.NET.LoadingSpace/FrmMain.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VIZCore3D.NET.LoadingSpace
+{
+    public partial class FrmMain : Form
+    {
+        private VIZCore3D.NET.VIZCore3DControl vizcore3d { get; set; }
+
+        private VIZCore3D.NET.Data.BoundBox3D ModelBoundBox { get; set; }
+
+        private List<VIZCore3D.NET.Data.Vector3D> Paths { get; set; }
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            VIZCore3D.NET.ModuleInitializer.Run();
+
+            vizcore3d = new VIZCore3DControl();
+            vizcore3d.Dock = DockStyle.Fill;
+            vizcore3d.OnInitializedVIZCore3D += VIZCore3D_OnInitializedVIZCore3D;
+            splitContainer1.Panel2.Controls.Add(vizcore3d);
+        }
+
+        private void VIZCore3D_OnInitializedVIZCore3D(object sender, EventArgs e)
+        {
+            VIZCore3D.NET.Data.LicenseResults result = 
+            vizcore3d.License.LicenseServer("127.0.0.1", 8901);
+
+            if(result != VIZCore3D.NET.Data.LicenseResults.SUCCESS)
+            {
+                MessageBox.Show("VIZCore3D License Error: {0}" + result.ToString());
+                return;
+            }
+
+            vizcore3d.View.SelectionMode = VIZCore3D.NET.Data.Object3DSelectionOptions.HIGHLIGHT_COLOR_AND_SILHOUETTEEDGE;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            vizcore3d.Model.OpenFileDialog();
+        }
+
+        private void txtBlock_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                if (String.IsNullOrEmpty(txtBlock.Text) == true) return;
+
+                List<VIZCore3D.NET.Data.Node> nodes = vizcore3d.Object3D.Find.QuickSearch(txtBlock.Text, true);
+
+                vizcore3d.Object3D.Select(nodes, true);
+            }
+        }
+
+        private void btnAnimatePath_Click(object sender, EventArgs e)
+        {
+            ModelBoundBox = vizcore3d.Model.BoundBox;
+
+            List<VIZCore3D.NET.Data.Node> nodes = vizcore3d.Object3D.Find.QuickSearch(txtBlock.Text, true);
+            VIZCore3D.NET.Data.BoundBox3D blockBoundBox = vizcore3d.Object3D.GetBoundBox(nodes);
+
+            Paths = new List<Data.Vector3D>();
+            for (int i = 0; i < 5; i++)
+            {
+                Paths.Add(new Data.Vector3D(0.0f, 0.0f, 1000.0f));
+            }
+
+            for (int i = 0; i < 25; i++)
+            {
+                Paths.Add(new Data.Vector3D(-1000.0f, 0.0f, 0.0f));
+            }
+
+            float lengthZ = blockBoundBox.MinZ + 5000.0f;
+            int countZ = (int)(lengthZ / 1000.0f);
+
+            for (int i = 0; i < countZ; i++)
+            {
+                Paths.Add(new Data.Vector3D(0.0f, 0.0f, -1000.0f));
+            }
+
+            VIZCore3D.NET.Data.Vertex3D start = blockBoundBox.GetCenter();
+            vizcore3d.ShapeDrawing.AddVertex(new List<VIZCore3D.NET.Data.Vertex3D>() { start.Clone() }, 0, Color.Black, 5.0f, 10.0f, true);
+
+            for (int i = 0; i < Paths.Count; i++)
+            {
+                start += Paths[i];
+                vizcore3d.ShapeDrawing.AddVertex(new List<VIZCore3D.NET.Data.Vertex3D>() { start.Clone() }, 0, Color.Black, 5.0f, 10.0f, true);
+            }
+
+            VIZCore3D.NET.Data.Matrix3D matrix = new VIZCore3D.NET.Data.Matrix3D();
+            matrix.Identity();
+            VIZCore3D.NET.Data.Vector3D move = new Data.Vector3D(0, 0, 0);
+            for (int i = 0; i < Paths.Count; i++)
+            {
+                //matrix.Identity();
+                //matrix.SetTranslate(Paths[i]);
+
+                move += Paths[i];
+
+                vizcore3d.BeginUpdate();
+                //vizcore3d.Object3D.Transform.Transform(nodes, matrix, false);
+                vizcore3d.Object3D.Transform.Move(nodes, move, true);
+                vizcore3d.EndUpdate();
+            }
+        }
+    }
+}
